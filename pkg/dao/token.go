6de17b07842aa32bca857b388b7194d362c96e1c
thxCode <thxcode0824@gmail.com>
2023-02-07 19:16:31 +0800

    refactor: remove useless dao util
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/dao/token.go
+++ pkg/dao/token.go
@@ -4,8 +4,6 @@ import (
 	"errors"
 
 	"github.com/seal-io/seal/pkg/dao/model"
-	"github.com/seal-io/seal/pkg/dao/model/predicate"
-	"github.com/seal-io/seal/pkg/dao/model/token"
 )
 
 func TokenCreates(mc model.ClientSet, input ...*model.Token) ([]*model.TokenCreate, error) {
@@ -37,34 +35,6 @@ func TokenCreates(mc model.ClientSet, input ...*model.Token) ([]*model.TokenCrea
 		// optional.
 		c.SetNillableExpiration(r.Expiration)
 		rrs[i] = c
-	}
-	return rrs, nil
-}
-
-func TokenDeletes(mc model.ClientSet, input ...*model.Token) ([]*model.TokenDelete, error) {
-	if len(input) == 0 {
-		return nil, errors.New("invalid input: empty list")
-	}
-
-	var rrs = make([]*model.TokenDelete, len(input))
-	for i, r := range input {
-		if r == nil {
-			return nil, errors.New("invalid input: nil entity")
-		}
-
-		// predicated.
-		var ps []predicate.Token
-		switch {
-		case r.ID.IsNaive():
-			ps = append(ps, token.ID(r.ID))
-		case r.CasdoorTokenName != "":
-			ps = append(ps, token.CasdoorTokenName(r.CasdoorTokenName))
-		}
-		if len(ps) == 0 {
-			return nil, errors.New("invalid input: illegal predicates")
-		}
-		rrs[i] = mc.Tokens().Delete().
-			Where(ps...)
 	}
 	return rrs, nil
 }
