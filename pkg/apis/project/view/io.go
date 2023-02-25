eecd4b9146cda3da341f30467966023f8fe394ed
thxCode <thxcode0824@gmail.com>
2023-02-25 23:03:11 +0800

    fix: failed to count applications of project
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/apis/project/view/io.go
+++ pkg/apis/project/view/io.go
@@ -1,11 +1,15 @@
 package view
 
 import (
+	"context"
 	"errors"
+	"net/http"
 
 	"github.com/seal-io/seal/pkg/apis/runtime"
 	"github.com/seal-io/seal/pkg/dao/model"
+	"github.com/seal-io/seal/pkg/dao/model/project"
 	"github.com/seal-io/seal/pkg/dao/types"
+	"github.com/seal-io/seal/utils/json"
 )
 
 // Basic APIs
@@ -23,13 +27,21 @@ func (r CreateRequest) Validate() error {
 	return nil
 }
 
+func (r CreateRequest) Model() *model.Project {
+	return &model.Project{
+		Name:        r.Name,
+		Description: r.Description,
+		Labels:      r.Labels,
+	}
+}
+
 type CreateResponse = model.Project
 
 type DeleteRequest = GetRequest
 
 type UpdateRequest struct {
 	ID          types.ID          `uri:"id"`
-	Name        string            `json:"name"`
+	Name        string            `json:"name,omitempty"`
 	Description string            `json:"description,omitempty"`
 	Labels      map[string]string `json:"labels,omitempty"`
 }
@@ -39,6 +51,15 @@ func (r UpdateRequest) Validate() error {
 		return errors.New("invalid id: blank")
 	}
 	return nil
+}
+
+func (r UpdateRequest) Model() *model.Project {
+	return &model.Project{
+		ID:          r.ID,
+		Name:        r.Name,
+		Description: r.Description,
+		Labels:      r.Labels,
+	}
 }
 
 type GetRequest struct {
@@ -73,9 +94,17 @@ type GetApplicationsRequest struct {
 	ID types.ID `uri:"id"`
 }
 
-func (r GetApplicationsRequest) Validate() error {
+func (r GetApplicationsRequest) ValidateWith(ctx context.Context, input any) error {
+	var modelClient = input.(model.ClientSet)
+
 	if !r.ID.Valid(0) {
 		return errors.New("invalid id: blank")
+	}
+	_, err := modelClient.Projects().Query().
+		Where(project.ID(r.ID)).
+		OnlyID(ctx)
+	if err != nil {
+		return runtime.Error(http.StatusNotFound, "invalid id: not found")
 	}
 
 	return nil
@@ -85,6 +114,22 @@ type GetApplicationResponse struct {
 	*model.Application `json:",inline"`
 
 	EnvironmentName string `json:"environmentName"`
+}
+
+func (o *GetApplicationResponse) MarshalJSON() ([]byte, error) {
+	type Alias GetApplicationResponse
+
+	// move `.Edges.Environment.Name` to `.EnvironmentName`.
+	if o.Edges.Environment != nil {
+		o.EnvironmentName = o.Edges.Environment.Name
+		o.Edges.Environment = nil // release
+	}
+
+	return json.Marshal(&struct {
+		*Alias `json:",inline"`
+	}{
+		Alias: (*Alias)(o),
+	})
 }
 
 type GetApplicationsResponse = []GetApplicationResponse
