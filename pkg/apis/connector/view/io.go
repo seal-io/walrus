3d05d943ae7ad4e709e2d013f881b1b688ff6c92
thxCode <thxcode0824@gmail.com>
2023-03-07 21:09:14 +0800

    refactor: connector apis
    - support batch deletion api
    
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/apis/connector/view/io.go
+++ pkg/apis/connector/view/io.go
@@ -3,11 +3,14 @@ package view
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/seal-io/seal/pkg/apis/runtime"
 	"github.com/seal-io/seal/pkg/dao/model"
 	"github.com/seal-io/seal/pkg/dao/types"
+	"github.com/seal-io/seal/pkg/platform"
+	"github.com/seal-io/seal/pkg/platform/operator"
 )
 
 // Basic APIs
@@ -16,9 +19,24 @@ type CreateRequest struct {
 	*model.ConnectorCreateInput `json:",inline"`
 }
 
-func (r *CreateRequest) Validate() error {
+func (r *CreateRequest) ValidateWith(ctx context.Context, input any) error {
 	if r.Name == "" {
 		return errors.New("invalid name: blank")
+	}
+
+	var entity = r.Model()
+	op, err := platform.GetOperator(ctx, operator.CreateOptions{
+		Connector: *entity,
+	})
+	if err != nil {
+		return fmt.Errorf("invalid connector config: %w", err)
+	}
+	connected, err := op.IsConnected(ctx)
+	if err != nil {
+		return fmt.Errorf("invalid connector: %w", err)
+	}
+	if !connected {
+		return errors.New("invalid connector: unreachable")
 	}
 
 	return nil
@@ -39,8 +57,6 @@ func (r *UpdateRequest) Validate() error {
 	return nil
 }
 
-type UpdateResponse = *model.ConnectorOutput
-
 type GetRequest struct {
 	*model.ConnectorQueryInput `uri:",inline"`
 }
@@ -55,6 +71,20 @@ func (r *GetRequest) Validate() error {
 type GetResponse = *model.ConnectorOutput
 
 // Batch APIs
+
+type CollectionDeleteRequest []*model.ConnectorQueryInput
+
+func (r CollectionDeleteRequest) Validate() error {
+	if len(r) == 0 {
+		return errors.New("invalid input: empty")
+	}
+	for _, i := range r {
+		if !i.ID.Valid(0) {
+			return errors.New("invalid id: blank")
+		}
+	}
+	return nil
+}
 
 type CollectionGetRequest struct {
 	runtime.RequestPagination `query:",inline"`
