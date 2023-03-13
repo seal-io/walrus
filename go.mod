2eea2e2ecf070ce11d370eb8db42dc6d7bbfdb05
thxCode <thxcode0824@gmail.com>
2023-03-13 18:03:46 +0800

    refactor: complete terraform resource type convert
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- go.mod
+++ go.mod
@@ -35,11 +35,12 @@ require (
 	golang.org/x/time v0.3.0
 	golang.org/x/tools v0.5.0
 	helm.sh/helm/v3 v3.11.1
-	k8s.io/api v0.26.1
-	k8s.io/apimachinery v0.26.1
-	k8s.io/client-go v0.26.1
+	k8s.io/api v0.26.2
+	k8s.io/apimachinery v0.26.2
+	k8s.io/client-go v0.26.2
 	k8s.io/klog v1.0.0
 	k8s.io/klog/v2 v2.80.1
+	k8s.io/kube-aggregator v0.26.2
 	k8s.io/utils v0.0.0-20221128185143-99ec85e7a448
 	sigs.k8s.io/controller-runtime v0.14.2
 )
@@ -271,12 +272,12 @@ require (
 	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/exp v0.0.0-20230116083435-1de6713980de // indirect
 	golang.org/x/mod v0.7.0 // indirect
-	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/oauth2 v0.1.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.4.0 // indirect
-	golang.org/x/term v0.4.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/api v0.102.0 // indirect
@@ -289,9 +290,9 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apiextensions-apiserver v0.26.1 // indirect
-	k8s.io/apiserver v0.26.1 // indirect
+	k8s.io/apiserver v0.26.2 // indirect
 	k8s.io/cli-runtime v0.26.0 // indirect
-	k8s.io/component-base v0.26.1 // indirect
+	k8s.io/component-base v0.26.2 // indirect
 	k8s.io/kube-openapi v0.0.0-20221012153701-172d655c2280 // indirect
 	k8s.io/kubectl v0.26.0 // indirect
 	oras.land/oras-go v1.2.2 // indirect
--- pkg/platformk8s/intercept/converter_tf.go
+++ pkg/platformk8s/intercept/converter_tf.go
@@ -1,12 +1,30 @@
 package intercept
 
 import (
+	"fmt"
+
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
 	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv1 "k8s.io/api/autoscaling/v1"
+	autoscalingv2 "k8s.io/api/autoscaling/v2"
+	autoscalingv2beta2 "k8s.io/api/autoscaling/v2beta2"
 	batchv1 "k8s.io/api/batch/v1"
 	batchv1beta1 "k8s.io/api/batch/v1beta1"
+	certificatesv1 "k8s.io/api/certificates/v1"
+	certificatesv1beta1 "k8s.io/api/certificates/v1beta1"
 	corev1 "k8s.io/api/core/v1"
+	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
+	networkingv1 "k8s.io/api/networking/v1"
+	policyv1 "k8s.io/api/policy/v1"
+	policyv1beta1 "k8s.io/api/policy/v1beta1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	schedulingv1 "k8s.io/api/scheduling/v1"
+	storagev1 "k8s.io/api/storage/v1"
+	storagev1beta1 "k8s.io/api/storage/v1beta1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	apiregistrationv1 "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
 )
 
 // Terraform returns Converter to convert Terraform provider resource type to raw Kubernetes GVK/GVR.
@@ -38,40 +56,177 @@ var tfConvert = terraformConvert{
 func init() {
 	// emit, transfer and record.
 	//
-	// only consider operableEnforcer types,
-	// from https://registry.terraform.io/providers/hashicorp/kubernetes/2.18.1.
+	// ref to https://registry.terraform.io/providers/hashicorp/kubernetes/2.18.1.
 	//
 	for _, alias := range []string{
-		"kubernetes_daemon_set_v1", "kubernetes_daemonset", "kubernetes_daemon_set",
+		// core
+		"kubernetes_namespace_v1", "kubernetes_namespace",
+		"kubernetes_service_v1", "kubernetes_service",
+		"kubernetes_service_account_v1", "kubernetes_service_account", "kubernetes_default_service_account_v1", "kubernetes_default_service_account",
+		"kubernetes_config_map_v1", "kubernetes_config_map", "kubernetes_config_map_v1_data",
+		"kubernetes_secret_v1", "kubernetes_secret",
+		"kubernetes_pod_v1", "kubernetes_pod",
+		"kubernetes_endpoints_v1", "kubernetes_endpoints",
+		"kubernetes_limit_range_v1", "kubernetes_limit_range",
+		"kubernetes_persistent_volume_v1", "kubernetes_persistent_volume",
+		"kubernetes_persistent_volume_claim_v1", "kubernetes_persistent_volume_claim",
+		"kubernetes_replication_controller_v1", "kubernetes_replication_controller",
+		"kubernetes_resource_quota_v1", "kubernetes_resource_quota",
+
+		// api registration
+		"kubernetes_api_service_v1", "kubernetes_api_service",
+
+		// apps
 		"kubernetes_deployment_v1", "kubernetes_deployment",
+		"kubernetes_daemon_set_v1", "kubernetes_daemonset", "kubernetes_daemon_set",
 		"kubernetes_stateful_set_v1", "kubernetes_stateful_set",
-		"kubernetes_stateful_set_v1", "kubernetes_stateful_set",
+
+		// batch
 		"kubernetes_cron_job_v1",
 		"kubernetes_cron_job",
 		"kubernetes_job_v1", "kubernetes_job",
-		"kubernetes_replication_controller_v1", "kubernetes_replication_controller",
-		"kubernetes_pod_v1", "kubernetes_pod",
+
+		// autoscaling
+		"kubernetes_horizontal_pod_autoscaler_v2",
+		"kubernetes_horizontal_pod_autoscaler_v2beta2",
+		"kubernetes_horizontal_pod_autoscaler_v1", "kubernetes_horizontal_pod_autoscaler",
+
+		// certificates
+		"kubernetes_certificate_signing_request_v1",
+		"kubernetes_certificate_signing_request",
+
+		// rbac
+		"kubernetes_role_v1", "kubernetes_role",
+		"kubernetes_role_binding_v1", "kubernetes_role_binding",
+		"kubernetes_cluster_role_v1", "kubernetes_cluster_role",
+		"kubernetes_cluster_role_binding_v1", "kubernetes_cluster_role_binding",
+
+		// networking
+		"kubernetes_ingress_v1",
+		"kubernetes_ingress",
+		"kubernetes_ingress_class_v1", "kubernetes_ingress_class",
+		"kubernetes_network_policy_v1", "kubernetes_network_policy",
+
+		// policy
+		"kubernetes_pod_disruption_budget_v1",
+		"kubernetes_pod_disruption_budget",
+		"kubernetes_pod_security_policy_v1beta1", "kubernetes_pod_security_policy",
+
+		// scheduling
+		"kubernetes_priority_class_v1", "kubernetes_priority_class",
+
+		// admission control
+		"kubernetes_validating_webhook_configuration_v1",
+		"kubernetes_validating_webhook_configuration",
+		"kubernetes_mutating_webhook_configuration_v1",
+		"kubernetes_mutating_webhook_configuration",
+
+		// storage
+		"kubernetes_storage_class_v1", "kubernetes_storage_class",
+		"kubernetes_csi_driver_v1", "kubernetes_csi_driver",
 	} {
 		var gvk = func() schema.GroupVersionKind {
 			switch alias {
-			case "kubernetes_daemon_set_v1", "kubernetes_daemonset", "kubernetes_daemon_set":
-				return appsv1.SchemeGroupVersion.WithKind("DaemonSet")
+			case "kubernetes_namespace_v1", "kubernetes_namespace":
+				return corev1.SchemeGroupVersion.WithKind("Namespace")
+			case "kubernetes_service_v1", "kubernetes_service":
+				return corev1.SchemeGroupVersion.WithKind("Service")
+			case "kubernetes_service_account_v1", "kubernetes_service_account", "kubernetes_default_service_account_v1", "kubernetes_default_service_account":
+				return corev1.SchemeGroupVersion.WithKind("ServiceAccount")
+			case "kubernetes_config_map_v1", "kubernetes_config_map", "kubernetes_config_map_v1_data":
+				return corev1.SchemeGroupVersion.WithKind("ConfigMap")
+			case "kubernetes_secret_v1", "kubernetes_secret":
+				return corev1.SchemeGroupVersion.WithKind("Secret")
+			case "kubernetes_pod_v1", "kubernetes_pod":
+				return corev1.SchemeGroupVersion.WithKind("Pod")
+			case "kubernetes_endpoints_v1", "kubernetes_endpoints":
+				return corev1.SchemeGroupVersion.WithKind("Endpoints")
+			case "kubernetes_limit_range_v1", "kubernetes_limit_range":
+				return corev1.SchemeGroupVersion.WithKind("LimitRange")
+			case "kubernetes_persistent_volume_v1", "kubernetes_persistent_volume":
+				return corev1.SchemeGroupVersion.WithKind("PersistentVolume")
+			case "kubernetes_persistent_volume_claim_v1", "kubernetes_persistent_volume_claim":
+				return corev1.SchemeGroupVersion.WithKind("PersistentVolumeClaim")
+			case "kubernetes_replication_controller_v1", "kubernetes_replication_controller":
+				return corev1.SchemeGroupVersion.WithKind("ReplicationController")
+			case "kubernetes_resource_quota_v1", "kubernetes_resource_quota":
+				return corev1.SchemeGroupVersion.WithKind("ResourceQuota")
+
+			case "kubernetes_api_service_v1", "kubernetes_api_service":
+				return apiregistrationv1.SchemeGroupVersion.WithKind("APIService")
+
 			case "kubernetes_deployment_v1", "kubernetes_deployment":
 				return appsv1.SchemeGroupVersion.WithKind("Deployment")
+			case "kubernetes_daemon_set_v1", "kubernetes_daemonset", "kubernetes_daemon_set":
+				return appsv1.SchemeGroupVersion.WithKind("DaemonSet")
 			case "kubernetes_stateful_set_v1", "kubernetes_stateful_set":
 				return appsv1.SchemeGroupVersion.WithKind("StatefulSet")
+
 			case "kubernetes_cron_job_v1":
 				return batchv1.SchemeGroupVersion.WithKind("CronJob")
 			case "kubernetes_cron_job":
 				return batchv1beta1.SchemeGroupVersion.WithKind("CronJob")
 			case "kubernetes_job_v1", "kubernetes_job":
 				return batchv1.SchemeGroupVersion.WithKind("Job")
-			case "kubernetes_replication_controller_v1", "kubernetes_replication_controller":
-				return corev1.SchemeGroupVersion.WithKind("ReplicationController")
-			case "kubernetes_pod_v1", "kubernetes_pod":
-				return corev1.SchemeGroupVersion.WithKind("Pod")
+
+			case "kubernetes_horizontal_pod_autoscaler_v2":
+				return autoscalingv2.SchemeGroupVersion.WithKind("HorizontalPodAutoscaler")
+			case "kubernetes_horizontal_pod_autoscaler_v2beta2":
+				return autoscalingv2beta2.SchemeGroupVersion.WithKind("HorizontalPodAutoscaler")
+			case "kubernetes_horizontal_pod_autoscaler_v1", "kubernetes_horizontal_pod_autoscaler":
+				return autoscalingv1.SchemeGroupVersion.WithKind("HorizontalPodAutoscaler")
+
+			case "kubernetes_certificate_signing_request_v1":
+				return certificatesv1.SchemeGroupVersion.WithKind("CertificateSigningRequest")
+			case "kubernetes_certificate_signing_request":
+				return certificatesv1beta1.SchemeGroupVersion.WithKind("CertificateSigningRequest")
+
+			case "kubernetes_role_v1", "kubernetes_role":
+				return rbacv1.SchemeGroupVersion.WithKind("Role")
+			case "kubernetes_role_binding_v1", "kubernetes_role_binding":
+				return rbacv1.SchemeGroupVersion.WithKind("RoleBinding")
+			case "kubernetes_cluster_role_v1", "kubernetes_cluster_role":
+				return rbacv1.SchemeGroupVersion.WithKind("ClusterRole")
+			case "kubernetes_cluster_role_binding_v1", "kubernetes_cluster_role_binding":
+				return rbacv1.SchemeGroupVersion.WithKind("ClusterRoleBinding")
+
+			case "kubernetes_ingress_v1":
+				return networkingv1.SchemeGroupVersion.WithKind("Ingress")
+			case "kubernetes_ingress":
+				return extensionsv1beta1.SchemeGroupVersion.WithKind("Ingress")
+			case "kubernetes_ingress_class_v1", "kubernetes_ingress_class":
+				return networkingv1.SchemeGroupVersion.WithKind("IngressClass")
+			case "kubernetes_network_policy_v1", "kubernetes_network_policy":
+				return networkingv1.SchemeGroupVersion.WithKind("NetworkPolicy")
+
+			case "kubernetes_pod_disruption_budget_v1":
+				return policyv1.SchemeGroupVersion.WithKind("PodDisruptionBudget")
+			case "kubernetes_pod_disruption_budget":
+				return policyv1beta1.SchemeGroupVersion.WithKind("PodDisruptionBudget")
+			case "kubernetes_pod_security_policy_v1beta1", "kubernetes_pod_security_policy":
+				return policyv1beta1.SchemeGroupVersion.WithKind("PodSecurityPolicy")
+
+			case "kubernetes_priority_class_v1", "kubernetes_priority_class":
+				return schedulingv1.SchemeGroupVersion.WithKind("PriorityClass")
+
+			case "kubernetes_validating_webhook_configuration_v1":
+				return admissionregistrationv1.SchemeGroupVersion.WithKind("ValidatingWebhookConfiguration")
+			case "kubernetes_validating_webhook_configuration":
+				return admissionregistrationv1beta1.SchemeGroupVersion.WithKind("ValidatingWebhookConfiguration")
+			case "kubernetes_mutating_webhook_configuration_v1":
+				return admissionregistrationv1.SchemeGroupVersion.WithKind("MutatingWebhookConfiguration")
+			case "kubernetes_mutating_webhook_configuration":
+				return admissionregistrationv1beta1.SchemeGroupVersion.WithKind("MutatingWebhookConfiguration")
+
+			case "kubernetes_storage_class_v1", "kubernetes_storage_class":
+				return storagev1.SchemeGroupVersion.WithKind("StorageClass")
+			case "kubernetes_csi_driver_v1":
+				return storagev1.SchemeGroupVersion.WithKind("CSIDriver")
+			case "kubernetes_csi_driver":
+				return storagev1beta1.SchemeGroupVersion.WithKind("CSIDriver")
 			}
-			panic("it will never happen")
+
+			panic(fmt.Sprintf("needs transferring new alias %s to Kubernetes GVK", alias))
 		}()
 		tfConvert.gvkm[alias] = gvk
 		tfConvert.gvrm[alias], _ = meta.UnsafeGuessKindToResource(gvk)
