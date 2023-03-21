c36ee044fdc57edccc38cb405a927dd7d59f371a
thxCode <thxcode0824@gmail.com>
2023-03-21 10:33:40 +0800

    refactor: extra cache interface
    - adapt cache interface with bigcache
    
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- staging/utils/cache/cache.go
+++ staging/utils/cache/cache.go
@@ -2,59 +2,84 @@ package cache
 
 import (
 	"context"
-	"fmt"
-	"time"
-
-	"github.com/allegro/bigcache/v3"
-	"github.com/dustin/go-humanize"
-
-	"github.com/seal-io/seal/utils/log"
+	"errors"
+	"io"
+	"strings"
 )
 
-func MustNew(ctx context.Context) *bigcache.BigCache {
-	var n, err = New(ctx)
-	if err != nil {
-		panic(fmt.Errorf("error creating cache: %w", err))
-	}
-	return n
+var (
+	ErrEntryNotFound  = errors.New("entry is not found")
+	ErrEntryTooBigger = errors.New("entry is too bigger")
+)
+
+// Entry holds the action of cache entry.
+type Entry interface {
+	// Key returns entry key.
+	Key() string
+
+	// Value returns entry value.
+	Value() ([]byte, error)
 }
 
-func New(ctx context.Context) (*bigcache.BigCache, error) {
-	// each shard initializes with `(MaxEntriesInWindows / Shards) * MaxEntrySize` = 300 * 512 = 150kb
-	// each shard limits in `(HardMaxCacheSize * 1024 * 1024) / Shards` = 64 * 1024 * 1024 / 64 = 1mb
-	// initializes with 64 * 150kb = 9mb, limits with 64 * 1mb = 64mb.
-	var cfg = bigcache.Config{
-		LifeWindow:         15 * time.Minute,
-		CleanWindow:        3 * time.Minute,
-		Shards:             64,
-		MaxEntriesInWindow: 64 * 5 * 60,
-		MaxEntrySize:       512,
-		HardMaxCacheSize:   64,
-		StatsEnabled:       false,
-		Verbose:            false,
-	}
-	return NewWithConfig(ctx, cfg)
+// EntryKeyMatcher holds the predication of the iteration.
+type EntryKeyMatcher interface {
+	// Match returns true if the given key matched.
+	Match(key string) bool
 }
 
-func NewWithConfig(ctx context.Context, cfg bigcache.Config) (*bigcache.BigCache, error) {
-	if cfg.Logger == nil {
-		cfg.Logger = log.WithName("cache")
-	}
-	if cfg.OnRemoveWithReason == nil {
-		cfg.OnRemoveWithReason = func(key string, entry []byte, reason bigcache.RemoveReason) {
-			var desc = "unknown"
-			switch reason {
-			case bigcache.Deleted:
-				desc = "deleted"
-			case bigcache.Expired:
-				desc = "expired"
-			case bigcache.NoSpace:
-				desc = "nospace"
-			}
-			var size = humanize.IBytes(uint64(len(entry)))
-			cfg.Logger.Printf("%s: %10s | %s", desc, size, key)
-		}
-	}
+// HasPrefix implements the EntryKeyMatcher stereotype,
+// which means the key has the given prefix.
+type HasPrefix string
 
-	return bigcache.New(ctx, cfg)
+func (s HasPrefix) Match(key string) bool {
+	return strings.HasPrefix(key, string(s))
+}
+
+// HasSuffix implements the EntryKeyMatcher stereotype,
+// which means the key has the given suffix.
+type HasSuffix string
+
+func (s HasSuffix) Match(key string) bool {
+	return strings.HasSuffix(key, string(s))
+}
+
+// HasPartial implements the EntryKeyMatcher stereotype,
+// which means the key has the given partial.
+type HasPartial string
+
+func (s HasPartial) Match(key string) bool {
+	return strings.Contains(key, string(s))
+}
+
+// EntryAccessor accesses the entry during iterating,
+// it can break the iteration with a false returning or none nil error.
+type EntryAccessor func(ctx context.Context, entry Entry) (next bool, err error)
+
+// Cache holds the action of caching.
+type Cache interface {
+	io.Closer
+
+	// Set saves entry with the given key,
+	// it returns an ErrEntryTooBigger when entry is too bigger.
+	Set(ctx context.Context, key string, entry []byte) error
+
+	// Delete removes the keys.
+	Delete(ctx context.Context, keys ...string) error
+
+	// Get reads entry for the key,
+	// it returns an ErrEntryNotFound when no entry exists for the given key.
+	Get(ctx context.Context, key string) ([]byte, error)
+
+	// List reads entries for the key list.
+	List(ctx context.Context, keys ...string) (entries [][]byte, err error)
+
+	// Iterate iterates all entries of the whole cache,
+	// breaks with false returning or none nil error,
+	// do not do time-expensive callback during iteration.
+	Iterate(ctx context.Context, m EntryKeyMatcher, a EntryAccessor) error
+}
+
+// Underlay gets the underlay client.
+type Underlay[T any] interface {
+	Underlay() T
 }
---
+++ staging/utils/cache/memory.go
@@ -0,0 +1,270 @@
+package cache
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/allegro/bigcache/v3"
+	"github.com/dustin/go-humanize"
+
+	"github.com/seal-io/seal/utils/log"
+)
+
+// MemoryConfig holds the configuration of the in-memory cache,
+// entry indexes by key and stores in one bucket,
+// the total cache size is BucketCapacity * Buckets.
+type MemoryConfig struct {
+	// Namespace indicates the operating workspace.
+	Namespace string
+	// EntryMaxAge indicates the maximum lifetime of each entry,
+	// default is 15 mins.
+	EntryMaxAge time.Duration
+	// LazyEntryEviction indicates to evict an expired entry at next peeking,
+	// by default, a background looping tries to evict expired entries per 3 mins.
+	LazyEntryEviction bool
+	// Buckets indicates the bucket number of cache,
+	// value must be a power of two,
+	// default is 64.
+	Buckets int
+	// BucketCapacity indicates the maximum MB of each bucket,
+	// default is 1 MB.
+	BucketCapacity int
+	// LazyBucketScale indicates to scale when the current bucket is not enough to put a new entry,
+	// by default, create the bucket with the given capacity to avoid any array copying.
+	// It's worth noticing that the bucket capacity can not exceed even configured LazyBucketScale to true.
+	LazyBucketScale bool
+}
+
+func (c *MemoryConfig) Default() {
+	c.Namespace = strings.TrimSpace(c.Namespace)
+	if c.EntryMaxAge == 0 {
+		c.EntryMaxAge = 15 * time.Minute
+	}
+	if c.Buckets == 0 {
+		c.Buckets = 64
+	}
+	if c.BucketCapacity == 0 {
+		c.BucketCapacity = 1
+	}
+}
+
+func (c *MemoryConfig) Validate() error {
+	if c.EntryMaxAge < 0 {
+		return errors.New("invalid entry max age: negative")
+	}
+	if c.Buckets < 0 {
+		return errors.New("invalid buckets: negative")
+	}
+	if c.BucketCapacity < 0 {
+		return errors.New("invalid bucket capacity: negative")
+	}
+	return nil
+}
+
+// NewMemory returns an in-memory Cache implementation.
+func NewMemory(ctx context.Context) (Cache, error) {
+	return NewMemoryWithConfig(ctx, MemoryConfig{})
+}
+
+// MustNewMemory likes NewMemory, but panic if error found.
+func MustNewMemory(ctx context.Context) Cache {
+	var n, err = NewMemory(ctx)
+	if err != nil {
+		panic(fmt.Errorf("error creating in-memory cache: %w", err))
+	}
+	return n
+}
+
+// NewMemoryWithConfig returns an in-memory Cache implementation with given configuration.
+func NewMemoryWithConfig(ctx context.Context, cfg MemoryConfig) (Cache, error) {
+	// default, validate.
+	cfg.Default()
+	var err = cfg.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	// generate bigcache configuration with MemoryConfig.
+	//
+	// example bigcache config:
+	// bigcache.Config{
+	//		LifeWindow:         15 * time.Minute,
+	//		CleanWindow:        3 * time.Minute,
+	//		Shards:             64,
+	//		MaxEntriesInWindow: 64 * 5 * 60,  // works with MaxEntrySize to determinate the cache initialization.
+	//		MaxEntrySize:       512,
+	//		HardMaxCacheSize:   64,
+	//		StatsEnabled:       false,
+	//		Verbose:            false,
+	//	}
+	// each shard initializes with `(MaxEntriesInWindows / Shards) * MaxEntrySize` = 300 * 512 = 150kb
+	// each shard limits in `(HardMaxCacheSize * 1024 * 1024) / Shards` = 64 * 1024 * 1024 / 64 = 1mb
+	// initializes with 64 * 150kb = 9mb, limits with 64 * 1mb = 64mb.
+	//
+	var capacity = cfg.BucketCapacity * cfg.Buckets
+	var logger = log.WithName("cache").WithName("memory")
+	var underlayCfg = bigcache.Config{
+		Shards:             cfg.Buckets,
+		LifeWindow:         cfg.EntryMaxAge,
+		CleanWindow:        0,
+		MaxEntriesInWindow: cfg.Buckets,
+		MaxEntrySize:       capacity,
+		HardMaxCacheSize:   capacity,
+		StatsEnabled:       false,
+		Verbose:            false,
+		Logger:             logger,
+		OnRemoveWithReason: func(key string, entry []byte, reason bigcache.RemoveReason) {
+			var desc = "unknown"
+			switch reason {
+			case bigcache.Deleted:
+				desc = "deleted"
+			case bigcache.Expired:
+				desc = "expired"
+			case bigcache.NoSpace:
+				desc = "nospace"
+			}
+			var size = humanize.IBytes(uint64(len(entry)))
+			if len(key) > 10 {
+				logger.Debugf("%s: %10s | %10s...", desc, size, key[:10])
+			} else {
+				logger.Debugf("%s: %10s | %10s", desc, size, key)
+			}
+		},
+	}
+	if !cfg.LazyEntryEviction {
+		// set up a background looping to clean.
+		underlayCfg.CleanWindow = 3 * time.Minute
+	}
+	if cfg.LazyBucketScale {
+		// initialize the cache queue in 1/4 capacity.
+		underlayCfg.MaxEntrySize >>= 2
+	}
+
+	// init
+	underlay, err := bigcache.New(ctx, underlayCfg)
+	if err != nil {
+		return nil, err
+	}
+	var mc = memoryCache{
+		underlay: underlay,
+	}
+	if cfg.Namespace != "" {
+		var ns = cfg.Namespace + "#"
+		mc.namespace = &ns
+	}
+	return mc, nil
+}
+
+// MustNewMemoryWithConfig likes NewMemoryWithConfig, but panic if error found.
+func MustNewMemoryWithConfig(ctx context.Context, cfg MemoryConfig) Cache {
+	var n, err = NewMemoryWithConfig(ctx, cfg)
+	if err != nil {
+		panic(fmt.Errorf("error creating in-memory cache: %w", err))
+	}
+	return n
+}
+
+// memoryCache adapts Cache interface to implement an in-memory cache with bigcache.BigCache.
+type memoryCache struct {
+	namespace *string
+	underlay  *bigcache.BigCache
+}
+
+func (c memoryCache) wrapKey(s *string) *string {
+	if c.namespace == nil {
+		return s
+	}
+	var r = *c.namespace + *s
+	return &r
+}
+
+func (c memoryCache) Close() error {
+	return c.underlay.Close()
+}
+
+func (c memoryCache) Set(ctx context.Context, key string, entry []byte) (err error) {
+	var wk = c.wrapKey(&key)
+	err = c.underlay.Set(*wk, entry)
+	if err != nil && err.Error() == "entry is bigger than max shard size" {
+		err = ErrEntryTooBigger
+	}
+	return
+}
+
+func (c memoryCache) Delete(ctx context.Context, keys ...string) (err error) {
+	for i := range keys {
+		var wk = c.wrapKey(&keys[i])
+		err = c.underlay.Delete(*wk)
+		if err != nil && !errors.Is(err, bigcache.ErrEntryNotFound) {
+			return
+		}
+	}
+	return
+}
+
+func (c memoryCache) Get(ctx context.Context, key string) (entry []byte, err error) {
+	var wk = c.wrapKey(&key)
+	entry, err = c.underlay.Get(*wk)
+	if err != nil && errors.Is(err, bigcache.ErrEntryNotFound) {
+		err = ErrEntryNotFound
+	}
+	return
+}
+
+func (c memoryCache) List(ctx context.Context, keys ...string) (entries [][]byte, err error) {
+	entries = make([][]byte, len(keys))
+	for i := range keys {
+		entries[i], err = c.Get(ctx, keys[i])
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func (c memoryCache) Iterate(ctx context.Context, m EntryKeyMatcher, a EntryAccessor) error {
+	if a == nil {
+		return nil
+	}
+	var it = c.underlay.Iterator()
+	for it.SetNext() {
+		var e, err = it.Value()
+		if err != nil {
+			return err
+		}
+		var k = e.Key()
+		if c.namespace != nil && !strings.HasPrefix(k, *c.namespace) {
+			continue
+		}
+		if m != nil && !m.Match(k) {
+			continue
+		}
+		n, err := a(ctx, bigcacheEntry{i: e})
+		if err != nil {
+			return err
+		}
+		if !n {
+			break
+		}
+	}
+	return nil
+}
+
+func (c memoryCache) Underlay() *bigcache.BigCache {
+	return c.underlay
+}
+
+type bigcacheEntry struct {
+	i bigcache.EntryInfo
+}
+
+func (e bigcacheEntry) Key() string {
+	return e.i.Key()
+}
+
+func (e bigcacheEntry) Value() ([]byte, error) {
+	return e.i.Value(), nil
+}
