c7745595d6e5517240c7598ee97662a2b47f016e
thxCode <thxcode0824@gmail.com>
2023-03-22 19:25:44 +0800

    test: fix out-of-order output
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/platformtf/config/config_test.go
+++ pkg/platformtf/config/config_test.go
@@ -29,41 +29,6 @@ func TestCreateConfigToBytes(t *testing.T) {
 var1    = "ami-0c55b159cbfafe1f0"
 `),
 		},
-		{
-			name: "test create config to bytes with attributes and child blocks",
-			option: CreateOptions{
-				TerraformOptions: &TerraformOptions{
-					Token:         "token",
-					Address:       "https://localhost:8080",
-					SkipTLSVerify: true,
-				},
-				VariableOptions: &VariableOptions{
-					Variables: map[string]interface{}{
-						"var1": "value1",
-						"var2": "value2",
-					},
-				},
-			},
-			expected: []byte(`terraform {
-  backend "http" {
-    address                = "https://localhost:8080"
-    password               = "token"
-    skip_cert_verification = true
-    update_method          = "PUT"
-    username               = "seal"
-  }
-}
-
-variable "var1" {
-  type = string
-}
-
-variable "var2" {
-  type = string
-}
-
-`),
-		},
 	}
 
 	for _, tt := range testCases {
