89d9b8fb5c27c14306fadcda5e43bfa29342596f
thxCode <thxcode0824@gmail.com>
2023-04-17 19:40:28 +0800

    refactor: dynamicert listener
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- staging/utils/dynamicert/cache.go
+++ staging/utils/dynamicert/cache.go
@@ -1,73 +1,14 @@
 package dynamicert
 
-import (
-	"context"
-	"errors"
-	"fmt"
-	"time"
+import "golang.org/x/crypto/acme/autocert"
 
-	"github.com/rancher/dynamiclistener"
-	"golang.org/x/crypto/acme/autocert"
-	core "k8s.io/api/core/v1"
-
-	"github.com/seal-io/seal/utils/json"
-)
-
+// Cache is used by Manager to store and retrieve previously obtained certificates
+// and other account data as opaque blobs.
+//
+// Cache implementations should not rely on the key naming pattern. Keys can
+// include any printable ASCII characters, except the following: \/:*?"<>|.
 type Cache = autocert.Cache
 
+// DirCache implements Cache using a directory on the local filesystem.
+// If the directory does not exist, it will be created with 0700 permissions.
 type DirCache = autocert.DirCache
-
-func cacheToStorage(cache Cache) dynamiclistener.TLSStorage {
-	return storage{cache: cache}
-}
-
-type storage struct {
-	cache Cache
-}
-
-func (s storage) Get() (*core.Secret, error) {
-	var ctx, cancel = context.WithTimeout(context.Background(), 15*time.Second)
-	defer cancel()
-
-	var bs, err = s.cache.Get(ctx, "secret")
-	if err != nil {
-		if errors.Is(err, autocert.ErrCacheMiss) {
-			return nil, nil
-		}
-		return nil, fmt.Errorf("error getting secret from cache: %w", err)
-	}
-	var secret core.Secret
-	err = json.Unmarshal(bs, &secret)
-	if err != nil {
-		return nil, fmt.Errorf("error unmarshalling secret: %w", err)
-	}
-	return &secret, nil
-}
-
-func (s storage) Update(secret *core.Secret) error {
-	if secret == nil {
-		return nil
-	}
-
-	var ctx, cancel = context.WithTimeout(context.Background(), 15*time.Second)
-	defer cancel()
-
-	var bs, err = json.Marshal(secret)
-	if err != nil {
-		return fmt.Errorf("error marshalling secret: %w", err)
-	}
-	err = s.cache.Put(ctx, "secret", bs)
-	if err != nil {
-		return fmt.Errorf("error caching secret: %w", err)
-	}
-
-	err = s.cache.Put(ctx, core.TLSCertKey, secret.Data[core.TLSCertKey])
-	if err != nil {
-		return fmt.Errorf("error caching %s: %w", core.TLSCertKey, err)
-	}
-	err = s.cache.Put(ctx, core.TLSPrivateKeyKey, secret.Data[core.TLSPrivateKeyKey])
-	if err != nil {
-		return fmt.Errorf("error caching %s: %w", core.TLSPrivateKeyKey, err)
-	}
-	return nil
-}
---
+++ staging/utils/dynamicert/helper.go
@@ -0,0 +1,267 @@
+package dynamicert
+
+import (
+	"bytes"
+	"context"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"math/big"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+
+	"golang.org/x/crypto/acme/autocert"
+
+	"github.com/seal-io/seal/utils/log"
+)
+
+func GenCA() (crypto.Signer, *x509.Certificate, error) {
+	// generate private key.
+	var key, err = ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// generate certificate.
+	var now = time.Now()
+	var x509Cert = &x509.Certificate{
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		NotBefore:             now.Add(-5 * time.Minute).UTC(),
+		NotAfter:              now.Add(time.Hour * 24 * 365 * 10).UTC(), // 10 years
+		SerialNumber:          new(big.Int).SetInt64(now.Unix()),
+		Subject: pkix.Name{
+			CommonName:   "dynamicert-ca@" + strconv.FormatInt(now.Unix(), 10),
+			Organization: []string{"seal.io"},
+		},
+	}
+	certDER, err := x509.CreateCertificate(rand.Reader, x509Cert, x509Cert, key.Public(), key)
+	if err != nil {
+		return nil, nil, err
+	}
+	x509Cert, err = x509.ParseCertificate(certDER)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	log.WithName("dynamicert").Debugf("generated CA %q, not before %v, not after %v",
+		x509Cert.Subject, x509Cert.NotBefore, x509Cert.NotAfter)
+	return key, x509Cert, nil
+}
+
+func GenServerCert(caKey crypto.Signer, caX509Cert *x509.Certificate, key crypto.Signer, server ...string) (*x509.Certificate, error) {
+	var (
+		dnsNames    []string
+		ipAddresses []net.IP
+	)
+	for i := range server {
+		if ip := net.ParseIP(server[i]); ip != nil {
+			ipAddresses = append(ipAddresses, ip)
+		} else {
+			dnsNames = append(dnsNames, server[i])
+		}
+	}
+
+	var now = time.Now()
+	var x509Cert = &x509.Certificate{
+		DNSNames:     dnsNames,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		IPAddresses:  ipAddresses,
+		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		NotAfter:     time.Now().Add(time.Hour * 24 * 92).UTC(), // 3 months
+		NotBefore:    caX509Cert.NotBefore,
+		SerialNumber: new(big.Int).SetInt64(now.Unix()),
+		Subject: pkix.Name{
+			CommonName:   "dynamicert@" + strconv.FormatInt(now.Unix(), 10),
+			Organization: []string{"seal.io"},
+		},
+	}
+	certDER, err := x509.CreateCertificate(rand.Reader, x509Cert, caX509Cert, key.Public(), caKey)
+	if err != nil {
+		return nil, err
+	}
+	x509Cert, err = x509.ParseCertificate(certDER)
+	if err != nil {
+		return nil, err
+	}
+
+	log.WithName("dynamicert").Debugf("generated server certificate %q signed by CA %q, not before %v, not after %v",
+		x509Cert.Subject, caX509Cert.Subject, x509Cert.NotBefore, x509Cert.NotAfter)
+	return x509Cert, nil
+}
+
+func LoadOrGenSelfSignedCA(ctx context.Context, cache Cache) (crypto.Signer, *x509.Certificate, error) {
+	var bs, err = cache.Get(ctx, "ca")
+	if err != nil {
+		if !errors.Is(err, autocert.ErrCacheMiss) {
+			return nil, nil, err
+		}
+		var key, x509Cert, err = GenCA()
+		if err != nil {
+			return nil, nil, err
+		}
+		bs, err = encodeTlsCertificate(&tls.Certificate{
+			PrivateKey: key,
+			Certificate: [][]byte{
+				x509Cert.Raw,
+			},
+		})
+		if err != nil {
+			return nil, nil, err
+		}
+		err = cache.Put(ctx, "ca", bs)
+		if err != nil {
+			return nil, nil, err
+		}
+		return key, x509Cert, nil
+	}
+	tlsCert, err := decodeTlsCertificate(bs, "")
+	if err != nil {
+		return nil, nil, err
+	}
+	return tlsCert.PrivateKey.(crypto.Signer), tlsCert.Leaf, nil
+}
+
+func encodeTlsCertificate(tlsCert *tls.Certificate) ([]byte, error) {
+	var buf bytes.Buffer
+
+	// encode private key.
+	switch k := tlsCert.PrivateKey.(type) {
+	case *ecdsa.PrivateKey:
+		var b, err = x509.MarshalECPrivateKey(k)
+		if err != nil {
+			return nil, err
+		}
+		var pb = &pem.Block{Type: "EC PRIVATE KEY", Bytes: b}
+		if err = pem.Encode(&buf, pb); err != nil {
+			return nil, err
+		}
+	case *rsa.PrivateKey:
+		var b = x509.MarshalPKCS1PrivateKey(k)
+		var pb = &pem.Block{Type: "RSA PRIVATE KEY", Bytes: b}
+		if err := pem.Encode(&buf, pb); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, errors.New("unknown private key type")
+	}
+
+	// encode public key.
+	for _, b := range tlsCert.Certificate {
+		var pb = &pem.Block{Type: "CERTIFICATE", Bytes: b}
+		if err := pem.Encode(&buf, pb); err != nil {
+			return nil, err
+		}
+	}
+
+	return buf.Bytes(), nil
+}
+
+func decodeTlsCertificate(data []byte, hostname string) (*tls.Certificate, error) {
+	// decode private key.
+	keyBlock, data := pem.Decode(data)
+	if keyBlock == nil || !strings.Contains(keyBlock.Type, "PRIVATE") {
+		return nil, errors.New("corrupt private key: invalid format")
+	}
+	var key crypto.PrivateKey
+	key, err := x509.ParseECPrivateKey(keyBlock.Bytes)
+	if err != nil {
+		// try RSA
+		var k, err = x509.ParsePKCS8PrivateKey(keyBlock.Bytes)
+		if err != nil {
+			return nil, errors.New("corrupt private key: parse failed")
+		}
+		key = k.(*rsa.PrivateKey)
+	}
+
+	// decode public key.
+	var certs [][]byte
+	for len(data) > 0 {
+		var certBlock *pem.Block
+		certBlock, data = pem.Decode(data)
+		if certBlock == nil {
+			break
+		}
+		certs = append(certs, certBlock.Bytes)
+	}
+	if len(data) > 0 {
+		return nil, errors.New("corrupt certificate: invalid format")
+	}
+
+	// get leaf certificate.
+	var n int
+	for i := range certs {
+		n += len(certs[i])
+	}
+	var der = make([]byte, n)
+	n = 0
+	for i := range certs {
+		n += copy(der[n:], certs[i])
+	}
+	x509Certs, err := x509.ParseCertificates(der)
+	if err != nil {
+		return nil, err
+	}
+	if len(x509Certs) == 0 {
+		return nil, errors.New("corrupt certificate: not found")
+	}
+	var leaf = x509Certs[0]
+
+	// validate leaf certificate.
+	err = verifyCert(leaf, hostname)
+	if err != nil {
+		return nil, err
+	}
+	switch prv := key.(type) {
+	case *ecdsa.PrivateKey:
+		var pub, ok = leaf.PublicKey.(*ecdsa.PublicKey)
+		if !ok {
+			return nil, errors.New("corrupt certificate: not ECDSA type")
+		}
+		if pub.X.Cmp(prv.X) != 0 || pub.Y.Cmp(prv.Y) != 0 {
+			return nil, errors.New("corrupt private key: not match ECDSA certificate")
+		}
+	case *rsa.PrivateKey:
+		var pub, ok = leaf.PublicKey.(*rsa.PublicKey)
+		if !ok {
+			return nil, errors.New("corrupt certificate: not RSA type")
+		}
+		if pub.N.Cmp(prv.N) != 0 {
+			return nil, errors.New("corrupt private key: not match RSA certificate")
+		}
+	}
+
+	return &tls.Certificate{
+		Certificate: certs,
+		PrivateKey:  key,
+		Leaf:        leaf,
+	}, nil
+}
+
+func verifyCert(x509Cert *x509.Certificate, hostname string) error {
+	var now = time.Now()
+	if now.Before(x509Cert.NotBefore) {
+		return errors.New("corrupt certificate: not valid yet")
+	}
+	if now.After(x509Cert.NotAfter) {
+		return errors.New("corrupt certificate: expired")
+	}
+	if hostname != "" {
+		var err = x509Cert.VerifyHostname(hostname)
+		if err != nil {
+			return fmt.Errorf("corrupt certificate: %w", err)
+		}
+	}
+	return nil
+}
