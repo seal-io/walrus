3d513673beb17fbe1f5ad5ff0ade15e10acc90a3
thxCode <thxcode0824@gmail.com>
2023-04-23 16:55:06 +0800

    feat: unify data storing
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/rds/local.go
+++ pkg/rds/local.go
@@ -4,10 +4,15 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"os/exec"
+	"path/filepath"
 	"time"
 
+	"github.com/seal-io/seal/pkg/consts"
+	"github.com/seal-io/seal/utils/files"
 	"github.com/seal-io/seal/utils/log"
+	"github.com/seal-io/seal/utils/strs"
 )
 
 const defaultDataSourceAddress = "postgres://root@127.0.0.1:5432/seal?sslmode=disable"
@@ -15,22 +20,34 @@ const defaultDataSourceAddress = "postgres://root@127.0.0.1:5432/seal?sslmode=di
 type Embedded struct{}
 
 func (Embedded) Run(ctx context.Context) error {
+	// create run data dir if not found.
+	var runDataPath = filepath.Join(consts.DataDir, "postgresql")
+	if !files.Exists(runDataPath) {
+		var err = files.Copy(
+			"/var/lib/postgresql/main",
+			runDataPath,
+			files.CopyWithTimes(),
+			files.CopyWithOwner())
+		if err != nil {
+			return fmt.Errorf("error copy initial data: %w", err)
+		}
+	}
+
 	const (
 		runUser = "postgres"
 		cmdName = "postgres"
 	)
+	var logger = log.WithName(cmdName)
 	var cmdArgs = []string{
 		runUser,
 		cmdName,
-		"-D",
-		"/var/lib/postgresql/main",
-		"-c",
-		"config_file=/etc/postgresql/main/postgresql.conf",
+		"-D", runDataPath,
+		"-c", "config_file=/etc/postgresql/main/postgresql.conf",
 	}
-	var cmd = exec.CommandContext(ctx, "gosu", cmdArgs...) // switch by gosu.
-	var logger = log.WithName(cmdName).V(5)
-	cmd.Stdout = logger
-	cmd.Stderr = logger
+	logger.Infof("run: gosu %s", strs.Join(" ", cmdArgs...))
+	var cmd = exec.CommandContext(ctx, "gosu", cmdArgs...)
+	cmd.Stdout = logger.V(5)
+	cmd.Stderr = logger.V(5)
 	var err = cmd.Run()
 	if err != nil && !errors.Is(err, context.Canceled) {
 		return err
