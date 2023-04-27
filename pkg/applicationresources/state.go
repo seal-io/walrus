5385c8df5ba9fc20aa3cdeba231eba13616b0fe9
thxCode <thxcode0824@gmail.com>
2023-04-27 08:58:28 +0800

    refactor: reuse operator at app resource stating
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/applicationresources/state.go
+++ pkg/applicationresources/state.go
@@ -6,24 +6,17 @@ import (
 	"go.uber.org/multierr"
 
 	"github.com/seal-io/seal/pkg/dao/model"
-	"github.com/seal-io/seal/pkg/dao/model/applicationresource"
-	"github.com/seal-io/seal/pkg/dao/model/connector"
 	"github.com/seal-io/seal/pkg/dao/types"
-	"github.com/seal-io/seal/pkg/platform"
 	"github.com/seal-io/seal/pkg/platform/operator"
 )
 
-// State gets status of the given model.ApplicationResource list according to its connector.
-func State(ctx context.Context, modelClient model.ClientSet, candidates []*model.ApplicationResource) (berr error) {
-	for i := range candidates {
-		// get operator.
-		var op, err = platform.GetOperator(ctx, operator.CreateOptions{
-			Connector: *candidates[i].Edges.Connector,
-		})
-		if multierr.AppendInto(&berr, err) {
-			continue
-		}
+// State gets status of the given model.ApplicationResource list with the given operator.Operator.
+func State(ctx context.Context, op operator.Operator, modelClient model.ClientSet, candidates []*model.ApplicationResource) (berr error) {
+	if op == nil {
+		return
+	}
 
+	for i := range candidates {
 		// get status of the application resource.
 		st, err := op.GetStatus(ctx, candidates[i])
 		if err != nil {
@@ -57,34 +50,4 @@ func State(ctx context.Context, modelClient model.ClientSet, candidates []*model
 		}
 	}
 	return
-}
-
-// ListStateCandidatesByPage gets the candidates for State by pagination params.
-func ListStateCandidatesByPage(ctx context.Context, modelClient model.ClientSet, offset, limit int) ([]*model.ApplicationResource, error) {
-	return queryStateCandidates(modelClient).
-		Offset(offset).
-		Limit(limit).
-		All(ctx)
-}
-
-func queryStateCandidates(modelClient model.ClientSet) *model.ApplicationResourceQuery {
-	return modelClient.ApplicationResources().Query().
-		Order(model.Desc(applicationresource.FieldCreateTime)).
-		Unique(false).
-		Select(
-			applicationresource.FieldID,
-			applicationresource.FieldStatus,
-			applicationresource.FieldInstanceID,
-			applicationresource.FieldConnectorID,
-			applicationresource.FieldType,
-			applicationresource.FieldName,
-			applicationresource.FieldDeployerType).
-		WithConnector(func(cq *model.ConnectorQuery) {
-			cq.Select(
-				connector.FieldName,
-				connector.FieldType,
-				connector.FieldCategory,
-				connector.FieldConfigVersion,
-				connector.FieldConfigData)
-		})
 }
--- pkg/scheduler/applicationresource/status_sync_task.go
+++ pkg/scheduler/applicationresource/status_sync_task.go
@@ -2,11 +2,19 @@ package applicationresource
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"time"
 
+	"go.uber.org/multierr"
+
 	"github.com/seal-io/seal/pkg/applicationresources"
 	"github.com/seal-io/seal/pkg/dao/model"
+	"github.com/seal-io/seal/pkg/dao/model/applicationresource"
+	"github.com/seal-io/seal/pkg/dao/model/connector"
+	"github.com/seal-io/seal/pkg/dao/types"
+	"github.com/seal-io/seal/pkg/platform"
+	"github.com/seal-io/seal/pkg/platform/operator"
 	"github.com/seal-io/seal/utils/gopool"
 	"github.com/seal-io/seal/utils/log"
 )
@@ -40,34 +48,118 @@ func (in *StatusSyncTask) Process(ctx context.Context, args ...interface{}) erro
 		in.logger.Debugf("processed in %v", time.Since(startTs))
 	}()
 
-	var cnt, err = in.modelClient.ApplicationResources().Query().
+	// NB(thxCode): in case of aggregate the status for application instance,
+	// we group 10 application instances into one task group,
+	// treat each application instance as a task unit,
+	// and then process resources stating in task unit.
+	var cnt, err = in.modelClient.ApplicationInstances().Query().
 		Count(ctx)
 	if err != nil {
-		return err
+		return fmt.Errorf("cannot count application instances: %w", err)
 	}
-
-	// divide processing buckets with count.
-	const bks = 100
+	if cnt == 0 {
+		return nil
+	}
+	// index none custom connectors for reusing.
+	cs, err := in.modelClient.Connectors().Query().
+		Select(
+			connector.FieldID,
+			connector.FieldName,
+			connector.FieldType,
+			connector.FieldCategory,
+			connector.FieldConfigVersion,
+			connector.FieldConfigData).
+		Where(connector.CategoryNEQ(types.ConnectorCategoryCustom)).
+		All(ctx)
+	if err != nil {
+		return fmt.Errorf("cannot list connectors: %w", err)
+	}
+	if len(cs) == 0 {
+		return nil
+	}
+	var ops = make(map[types.ID]operator.Operator, len(cs))
+	for i := range cs {
+		var op, err = platform.GetOperator(ctx, operator.CreateOptions{
+			Connector: *cs[i],
+		})
+		if err != nil {
+			// warn out without breaking the whole syncing.
+			in.logger.Infof("cannot get operator of connector %s: %v", cs[i].ID, err)
+			continue
+		}
+		ops[cs[i].ID] = op
+	}
+	// execute tasks.
+	const bks = 10
 	var bkc = cnt / bks
 	if bkc == 0 {
-		var st = in.buildStateTask(ctx, 0, bks)
+		var st = in.buildStateTasks(ctx, 0, bks, ops)
 		return st()
 	}
 	var wg = gopool.Group()
 	for bk := 0; bk < bkc; bk++ {
-		var st = in.buildStateTask(ctx, bk, bks)
+		var st = in.buildStateTasks(ctx, bk, bks, ops)
 		wg.Go(st)
 	}
 	return wg.Wait()
 }
 
-func (in *StatusSyncTask) buildStateTask(ctx context.Context, offset, limit int) func() error {
+func (in *StatusSyncTask) buildStateTasks(ctx context.Context, offset, limit int, ops map[types.ID]operator.Operator) func() error {
 	return func() error {
-		var entities, err = applicationresources.ListStateCandidatesByPage(
-			ctx, in.modelClient, offset, limit)
+		var is, err = in.modelClient.ApplicationInstances().Query().
+			Offset(offset).
+			Limit(limit).
+			All(ctx)
 		if err != nil {
-			return err
+			return fmt.Errorf("error listing application instances in pagination %d,%d: %w",
+				offset, limit, err)
 		}
-		return applicationresources.State(ctx, in.modelClient, entities)
+		var wg = gopool.Group()
+		for i := range is {
+			var at = in.buildStateTask(ctx, is[i], ops)
+			wg.Go(at)
+		}
+		return wg.Wait()
+	}
+}
+
+func (in *StatusSyncTask) buildStateTask(ctx context.Context, i *model.ApplicationInstance, ops map[types.ID]operator.Operator) func() error {
+	return func() (berr error) {
+		var rs, err = i.QueryResources().
+			Select(
+				applicationresource.FieldID,
+				applicationresource.FieldStatus,
+				applicationresource.FieldInstanceID,
+				applicationresource.FieldConnectorID,
+				applicationresource.FieldType,
+				applicationresource.FieldName,
+				applicationresource.FieldDeployerType).
+			All(ctx)
+		if err != nil {
+			return fmt.Errorf("error listing application resources: %w", err)
+		}
+		if len(rs) == 0 {
+			return
+		}
+
+		var ids = make(map[types.ID][]*model.ApplicationResource)
+		for y := range rs {
+			// group resources by connector.
+			ids[rs[y].ConnectorID] = append(ids[rs[y].ConnectorID],
+				rs[y])
+		}
+
+		for cid, crs := range ids {
+			var op, exist = ops[cid]
+			if !exist {
+				// ignore if not found operator.
+				continue
+			}
+			err = applicationresources.State(ctx, op, in.modelClient, crs)
+			if multierr.AppendInto(&berr, err) {
+				continue
+			}
+		}
+		return
 	}
 }
