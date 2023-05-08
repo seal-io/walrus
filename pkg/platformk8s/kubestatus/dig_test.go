aabc0c2408c4301bad3282fb0dc2239d211b17a0
thxCode <thxcode0824@gmail.com>
2023-05-08 11:29:54 +0800

    refactor: enhance pod error status message
    Signed-off-by: thxCode <thxcode0824@gmail.com>

---
+++ pkg/platformk8s/kubestatus/dig_test.go
@@ -0,0 +1,148 @@
+package kubestatus
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/json"
+)
+
+func Test_digPodErrorReason(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		given    core.PodStatus
+		expected string
+	}{
+		{
+			name: "init container exit without 0",
+			given: core.PodStatus{
+				InitContainerStatuses: []core.ContainerStatus{
+					{
+						Name: "init-volume",
+						State: core.ContainerState{
+							Terminated: &core.ContainerStateTerminated{
+								ExitCode: 0,
+							},
+						},
+					},
+					{
+						Name: "init-data",
+						State: core.ContainerState{
+							Terminated: &core.ContainerStateTerminated{
+								ExitCode: 1,
+							},
+						},
+					},
+				},
+			},
+			expected: `Init Container "init-data": exit code: 1`,
+		},
+		{
+			name: "init container terminate by signal",
+			given: core.PodStatus{
+				InitContainerStatuses: []core.ContainerStatus{
+					{
+						Name: "init-data",
+						State: core.ContainerState{
+							Terminated: &core.ContainerStateTerminated{
+								ExitCode: 1,
+								Signal:   9,
+							},
+						},
+					},
+				},
+			},
+			expected: `Init Container "init-data": signal: 9`,
+		},
+		{
+			name: "init container terminate by signal with reason",
+			given: core.PodStatus{
+				InitContainerStatuses: []core.ContainerStatus{
+					{
+						Name: "init-data",
+						State: core.ContainerState{
+							Terminated: &core.ContainerStateTerminated{
+								ExitCode: 1,
+								Signal:   9,
+								Message:  "exhaust all inodes",
+								Reason:   "Exhausted",
+							},
+						},
+					},
+				},
+			},
+			expected: `Init Container "init-data": Exhausted, exhaust all inodes`,
+		},
+		{
+			name: "init container waiting",
+			given: core.PodStatus{
+				InitContainerStatuses: []core.ContainerStatus{
+					{
+						Name: "init-volume",
+						State: core.ContainerState{
+							Waiting: &core.ContainerStateWaiting{
+								Message: `Back-off pulling image "volume-path-parser:apline"`,
+								Reason:  "ImagePullBackOff",
+							},
+						},
+					},
+					{
+						Name: "init-data",
+						State: core.ContainerState{
+							Terminated: &core.ContainerStateTerminated{
+								ExitCode: 1,
+							},
+						},
+					},
+				},
+			},
+			expected: `Init Container "init-volume": ImagePullBackOff, Back-off pulling image "volume-path-parser:apline"`,
+		},
+		{
+			name: "container terminate by signal",
+			given: core.PodStatus{
+				ContainerStatuses: []core.ContainerStatus{
+					{
+						Name: "wordpress",
+						State: core.ContainerState{
+							Terminated: &core.ContainerStateTerminated{
+								ExitCode: 1,
+							},
+						},
+					},
+				},
+			},
+			expected: `Container "wordpress": exit code: 1`,
+		},
+		{
+			name: "container waiting",
+			given: core.PodStatus{
+				ContainerStatuses: []core.ContainerStatus{
+					{
+						Name: "nginx",
+						State: core.ContainerState{
+							Waiting: &core.ContainerStateWaiting{
+								Message: `Back-off pulling image "nginx:apline"`,
+								Reason:  "ImagePullBackOff",
+							},
+						},
+					},
+				},
+			},
+			expected: `Container "nginx": ImagePullBackOff, Back-off pulling image "nginx:apline"`,
+		},
+	}
+	for _, tc := range testCases {
+		// convert object to map.
+		var given map[string]interface{}
+		var bs, _ = json.Marshal(tc.given)
+		_ = json.Unmarshal(bs, &given)
+
+		t.Run(tc.name, func(t *testing.T) {
+			var actual = digPodErrorReason(context.Background(), given)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
