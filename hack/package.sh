163c6e17ff6ea48518b75a08fe7d77f61b6829b0
thxCode <thxcode0824@gmail.com>
2023-05-12 18:15:30 +0800

    feat: manifest for kuberenetes deploying
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- hack/package.sh
+++ hack/package.sh
@@ -13,7 +13,7 @@ PACKAGE_TMP_DIR="${PACKAGE_DIR}/tmp"
 mkdir -p "${PACKAGE_TMP_DIR}"
 
 function download_ui() {
-  local default_tag="v0.1.1"
+  local default_tag="v0.1.2"
   local path="${1}"
   local tag="${2}"
 
