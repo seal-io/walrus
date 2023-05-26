ff66af3277fc4d0c54431de09ec2e2ef6677d0bd
thxCode <thxcode0824@gmail.com>
2023-05-26 15:10:31 +0800

    fix: failed deploy when disabling authn
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/auths/authn.go
+++ pkg/auths/authn.go
@@ -7,6 +7,7 @@ import (
 	"github.com/seal-io/seal/pkg/auths/session"
 	"github.com/seal-io/seal/pkg/auths/token"
 	"github.com/seal-io/seal/pkg/dao/model"
+	"github.com/seal-io/seal/pkg/dao/model/subject"
 	"github.com/seal-io/seal/pkg/dao/types"
 )
 
@@ -42,11 +43,21 @@ func authn(c *gin.Context, mc model.ClientSet, s session.Subject) (session.Subje
 	return s, nil
 }
 
-func authnSkip(_ *gin.Context, _ model.ClientSet, _ session.Subject) (session.Subject, error) {
-	// Mock admin.
-	return session.Subject{
-		Domain: types.SubjectDomainBuiltin,
-		Groups: []string{},
-		Name:   "admin",
-	}, nil
+func authnSkip(c *gin.Context, mc model.ClientSet, s session.Subject) (session.Subject, error) {
+	sid, err := mc.Subjects().Query().
+		Where(
+			subject.Kind(types.SubjectKindUser),
+			subject.Domain(types.SubjectDomainBuiltin),
+			subject.Name("admin")).
+		OnlyID(c)
+	if err != nil {
+		return s, err
+	}
+
+	s.ID = sid
+	s.Domain = types.SubjectDomainBuiltin
+	s.Groups = []string{}
+	s.Name = "admin"
+
+	return s, nil
 }
