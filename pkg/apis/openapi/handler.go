89b2d5ae178fecfc8e85421130f0dfa79ea0be13
michelia <michelia@seal.io>
2023-06-09 12:22:51 +0800

    refactor: update openapi schema package

--- pkg/apis/openapi/handler.go
+++ pkg/apis/openapi/handler.go
@@ -5,48 +5,53 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/gin-gonic/gin"
-	"github.com/ogen-go/ogen"
 
 	"github.com/seal-io/seal/pkg/apis/runtime"
 	"github.com/seal-io/seal/pkg/auths"
 	"github.com/seal-io/seal/utils/bytespool"
-	"github.com/seal-io/seal/utils/json"
-	"github.com/seal-io/seal/utils/version"
 )
 
 func Index(authing bool, apiPrefix string) runtime.Handle {
-	spec := runtime.OpenAPI(ogen.NewInfo().
-		SetTitle("Seal APIs").
-		SetDescription("API to manage resources of Seal").
-		SetVersion(version.Version))
+	spec := runtime.OpenAPI(&openapi3.Info{
+		Title:       "Seal APIs",
+		Description: "API to manage resources of Seal",
+		Version:     "version.Version",
+	})
 
 	if apiPrefix != "" {
-		spec.AddServers(ogen.NewServer().SetURL(apiPrefix))
+		spec.AddServer(&openapi3.Server{
+			URL: apiPrefix,
+		})
 	}
 
 	if authing {
-		securities := ogen.SecurityRequirements{
+		securities := openapi3.SecurityRequirements{
 			{"bearerAuth": {}},
 			{"cookieAuth": {}},
 		}
 		spec.Security = securities
-		securitySchemes := make(map[string]*ogen.SecurityScheme, 2)
-		securitySchemes["bearerAuth"] = &ogen.SecurityScheme{
-			Type:         "http",
-			In:           "header",
-			Scheme:       "bearer",
-			BearerFormat: "JWT",
+		securitySchemes := make(map[string]*openapi3.SecuritySchemeRef, 2)
+		securitySchemes["bearerAuth"] = &openapi3.SecuritySchemeRef{
+			Value: &openapi3.SecurityScheme{
+				Type:         "http",
+				In:           "header",
+				Scheme:       "bearer",
+				BearerFormat: "JWT",
+			},
 		}
-		securitySchemes["cookieAuth"] = &ogen.SecurityScheme{
-			Type: "apiKey",
-			In:   "cookie",
-			Name: auths.SessionCookieName,
+		securitySchemes["cookieAuth"] = &openapi3.SecuritySchemeRef{
+			Value: &openapi3.SecurityScheme{
+				Type: "apiKey",
+				In:   "cookie",
+				Name: auths.SessionCookieName,
+			},
 		}
 
 		spec.Components.SecuritySchemes = securitySchemes
 		for i := range spec.Paths {
-			ops := []*ogen.Operation{
+			ops := []*openapi3.Operation{
 				spec.Paths[i].Post,
 				spec.Paths[i].Delete,
 				spec.Paths[i].Put,
@@ -56,15 +61,12 @@ func Index(authing bool, apiPrefix string) runtime.Handle {
 				if ops[j] == nil {
 					continue
 				}
-				ops[j].Security = securities
+				ops[j].Security = &securities
 			}
 		}
 	}
 
-	// Add named schemas.
-	spec.AddNamedSchemas()
-
-	specBytes, err := json.Marshal(spec)
+	specBytes, err := spec.MarshalJSON()
 	if err != nil {
 		panic(fmt.Errorf("error marshaling openapi spec: %w", err))
 	}
