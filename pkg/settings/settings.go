8a4c2cda86033e10b4db89fc6ae05c6e895d845a
gitlawr <lawrleegle@gmail.com>
2023-06-21 16:35:07 +0800

    feat: bump deployer image

--- pkg/settings/settings.go
+++ pkg/settings/settings.go
@@ -42,8 +42,7 @@ var (
 		"DeployerImage",
 		editable,
 		// When the image is updated, sync the one in server Dockerfile.
-		// TODO change when deployer image with packaged plugins is ready.
-		initializeFrom("sealio/terraform-deployer:v0.1.2"),
+		initializeFrom("sealio/terraform-deployer:v0.1.3"),
 		modifyWith(notBlank, containerImageReference),
 	)
 )
