3012f2505ec394203d7a8c7d4fb30ca9161021dc
thxCode <thxcode0824@gmail.com>
2023-06-22 23:42:39 +0800

    refactor: bump casdoor version
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/casdoor/helper.go
+++ pkg/casdoor/helper.go
@@ -11,20 +11,32 @@ import (
 	"github.com/seal-io/seal/utils/req"
 )
 
-func Wait(ctx context.Context, serverUrl string) error {
+func Wait(ctx context.Context, serverUrl string) (err error) {
 	endpoint.Set(serverUrl)
-	logoutURL := fmt.Sprintf("%s/api/logout", serverUrl)
 
-	return wait.PollImmediateUntilWithContext(ctx, 2*time.Second,
+	var lastErr error
+
+	err = wait.PollImmediateUntilWithContext(ctx, 2*time.Second,
 		func(ctx context.Context) (bool, error) {
-			err := req.HTTPRequest().
-				Post(logoutURL).
-				Error()
-			if err != nil {
-				log.Warnf("waiting for casdoor to be ready: %v", err)
+			lastErr = IsConnected(ctx)
+			if lastErr != nil {
+				log.Warnf("waiting for casdoor to be ready: %v", lastErr)
 			}
 
-			return err == nil, ctx.Err()
+			return lastErr == nil, ctx.Err()
 		},
 	)
+	if err != nil && lastErr != nil {
+		err = lastErr // Use last error to overwrite context error while existed.
+	}
+
+	return
+}
+
+func IsConnected(ctx context.Context) error {
+	logoutURL := fmt.Sprintf("%s/api/health", endpoint.Get())
+
+	return req.HTTPRequest().
+		GetWithContext(ctx, logoutURL).
+		Error()
 }
