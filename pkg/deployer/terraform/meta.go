f951f94f7fc30ef946426f92a7db20993aed006a
gitlawr <lawrleegle@gmail.com>
2023-08-17 16:58:50 +0800

    refactor: update metadata attribute keys

--- pkg/deployer/terraform/meta.go
+++ pkg/deployer/terraform/meta.go
@@ -4,17 +4,15 @@ import "k8s.io/apimachinery/pkg/util/sets"
 
 // walrus metadata indicate walrus will set value to these attribute while user module include these attribute.
 const (
-	/* TODO coordinate with templates and update the following keys.*/
-
-	WalrusMetadataProjectName     = "seal_metadata_project_name"
-	WalrusMetadataEnvironmentName = "seal_metadata_environment_name"
-	WalrusMetadataServiceName     = "seal_metadata_service_name"
-	WalrusMetadataProjectID       = "seal_metadata_project_id"
-	WalrusMetadataEnvironmentID   = "seal_metadata_environment_id"
-	WalrusMetadataServiceID       = "seal_metadata_service_id"
+	WalrusMetadataProjectName     = "walrus_metadata_project_name"
+	WalrusMetadataEnvironmentName = "walrus_metadata_environment_name"
+	WalrusMetadataServiceName     = "walrus_metadata_service_name"
+	WalrusMetadataProjectID       = "walrus_metadata_project_id"
+	WalrusMetadataEnvironmentID   = "walrus_metadata_environment_id"
+	WalrusMetadataServiceID       = "walrus_metadata_service_id"
 	// WalrusMetadataNamespaceName is the managed namespace name of an environment,
 	// valid when Kubernetes connector is used in the environment.
-	WalrusMetadataNamespaceName = "seal_metadata_namespace_name"
+	WalrusMetadataNamespaceName = "walrus_metadata_namespace_name"
 )
 
 var WalrusMetadataSet = sets.NewString(
