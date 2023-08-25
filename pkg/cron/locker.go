33e94165b83cc81340afabac4b6b90897bded50f
thxCode <thxcode0824@gmail.com>
2023-08-25 15:50:28 +0800

    refactor: cron locker
    - fix leaking goroutine when holder lost its permission to renew the
      key.
    
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/cron/locker.go
+++ pkg/cron/locker.go
@@ -2,41 +2,42 @@ package cron
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
-	"strings"
 	"time"
 
 	"entgo.io/ent/dialect/sql/sqlgraph"
+	"k8s.io/apimachinery/pkg/util/wait"
 
 	"github.com/seal-io/walrus/pkg/dao/model"
 	"github.com/seal-io/walrus/pkg/dao/model/distributelock"
 	"github.com/seal-io/walrus/utils/cron"
 	"github.com/seal-io/walrus/utils/gopool"
 	"github.com/seal-io/walrus/utils/log"
+	"github.com/seal-io/walrus/utils/strs"
 	"github.com/seal-io/walrus/utils/version"
 )
 
 const (
 	defaultExpiry        = 15 * time.Second
-	defaultRenewInterval = 10 * time.Second
+	defaultRenewInterval = 5 * time.Second
 	maxExecuteDuration   = 15 * time.Minute
 )
 
 // NewLocker create locker with model client and options.
-func NewLocker(client *model.Client, options ...Option) *Locker {
+func NewLocker(logger log.Logger, client *model.Client, options ...Option) *Locker {
 	l := &Locker{
+		logger:             logger.WithName("locker"),
+		holder:             version.GetInstanceUUID(),
 		client:             client,
 		expiry:             defaultExpiry,
 		renewInterval:      defaultRenewInterval,
 		maxExecuteDuration: maxExecuteDuration,
-		logger:             log.GetLogger().WithName("locker"),
 	}
-
 	for _, opt := range options {
 		opt(l)
 	}
-
-	l.logger.Infof("created cronjob locker with instance uuid %s", version.GetInstanceUUID())
 
 	return l
 }
@@ -61,53 +62,51 @@ type Option func(*Locker)
 
 // Locker implement the cronjob go-co-op/gocron Locker interface.
 type Locker struct {
+	logger             log.Logger
+	holder             string
 	client             *model.Client
 	expiry             time.Duration
 	renewInterval      time.Duration
 	maxExecuteDuration time.Duration
-	logger             log.Logger
 }
 
 // Lock try to lock the key provided, return error while failed to lock key.
-func (l *Locker) Lock(ctx context.Context, key string) (lock cron.Lock, err error) {
-	l.logger.V(6).Infof("try to lock key %s", key)
-
-	defer func() {
-		if err != nil && !sqlgraph.IsUniqueConstraintError(err) && !strings.Contains(err.Error(), "is locked") {
-			l.logger.Warnf("error lock key %s: %v", key, err)
-		}
-	}()
-
-	err = l.createLock(ctx, key)
+func (l *Locker) Lock(ctx context.Context, key string) (cron.Lock, error) {
+	lk, err := l.acquire(ctx, key)
 	if err != nil {
+		var errLocked *lockedError
+
+		switch {
+		case !sqlgraph.IsUniqueConstraintError(err) && !errors.As(err, &errLocked):
+			l.logger.Warnf("error acquiring lock for %q: %v", key, err)
+		case errLocked != nil && errLocked.Holder == l.holder:
+			// NB(thxCode): time-cost operation, need to be optimized.
+			l.logger.WarnS("previous locking is not finished", "createdBy", key)
+		}
+
 		return nil, err
 	}
 
-	// Renew.
-	renewCtx, cancel := context.WithTimeout(ctx, l.maxExecuteDuration)
+	lk.logger.V(6).Info("succeeded lock")
 
-	gopool.Go(func() {
-		l.renew(renewCtx, key)
-	})
-
-	lock = &Lock{
-		key:         key,
-		client:      l.client,
-		logger:      l.logger,
-		cancelRenew: cancel,
-	}
-
-	l.logger.V(6).Infof("success lock key %s", key)
-
-	return
+	return lk, nil
 }
 
-// createLock create lock with key and store it in the db.
-func (l *Locker) createLock(ctx context.Context, key string) error {
+type lockedError struct {
+	Key, Holder string
+}
+
+func (e *lockedError) Error() string {
+	return fmt.Sprintf("key %q is locked by %q", e.Key, e.Holder)
+}
+
+// acquire try to get the lock of key provided,
+// return error while failed to acquire lock.
+func (l *Locker) acquire(ctx context.Context, key string) (*Lock, error) {
 	now := time.Now().Unix()
 	expiry := now + int64(l.expiry.Seconds())
 
-	return l.client.WithTx(ctx, func(tx *model.Tx) (err error) {
+	err := l.client.WithTx(ctx, func(tx *model.Tx) error {
 		// Check whether key is existed.
 		// Key may fail to unlock when exception occurred, like instance been shutdown.
 		current, err := tx.DistributeLock.Query().
@@ -119,136 +118,136 @@ func (l *Locker) createLock(ctx context.Context, key string) error {
 		}
 
 		if current != nil {
-			if float64(now-current.ExpireAt) >= l.expiry.Seconds() {
-				// Key expired.
-				err = tx.DistributeLock.DeleteOneID(key).
-					Exec(ctx)
-				if err != nil {
-					return err
+			// Key locked, will auto release after expired.
+			if float64(now-current.ExpireAt) < l.expiry.Seconds() {
+				return &lockedError{
+					Key:    key,
+					Holder: current.Holder,
 				}
-			} else {
-				if current.Holder == version.GetInstanceUUID() {
-					l.logger.Warnf("previous %s processing is not finished", key)
-				}
-				// Key locked, will auto release after expired.
-				return fmt.Errorf("key %s is locked", key)
+			}
+
+			// Key expired, release it.
+			err = tx.DistributeLock.DeleteOneID(key).
+				Exec(ctx)
+			if err != nil {
+				return err
 			}
 		}
 
 		// Create key.
-		_, err = tx.DistributeLock.Create().
+		return tx.DistributeLock.Create().
 			SetID(key).
 			SetExpireAt(expiry).
-			SetHolder(version.GetInstanceUUID()).
-			Save(ctx)
-		if err != nil {
-			return err
-		}
-
-		return
+			SetHolder(l.holder).
+			Exec(ctx)
 	})
-}
-
-func (l *Locker) renew(ctx context.Context, key string) {
-	ticker := time.NewTicker(l.renewInterval)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			err := l.client.WithTx(ctx, func(tx *model.Tx) (err error) {
-				// Get current key.
-				current, err := tx.DistributeLock.Query().
-					Where(distributelock.ID(key)).
-					Only(ctx)
-				if err != nil {
-					// Key already unlocked.
-					if model.IsNotFound(err) {
-						return nil
-					}
-
-					return err
-				}
-
-				if current == nil || current.Holder != version.GetInstanceUUID() ||
-					current.ExpireAt < time.Now().Unix() {
-					// Should not renew.
-					return nil
-				}
-
-				// Renew key.
-				expiry := time.Now().Unix() + int64(l.expiry.Seconds())
-
-				_, err = tx.DistributeLock.Update().
-					Where(distributelock.ID(key)).
-					SetExpireAt(expiry).
-					Save(ctx)
-				if err != nil && !model.IsNotFound(err) {
-					return err
-				}
-
-				l.logger.V(6).Infof("success renew key %s", key)
-
-				return nil
-			})
-			if err != nil {
-				l.logger.Warnf("error renew key %s: %v", key, err)
-			}
-		case <-ctx.Done():
-			return
-		}
+	if err != nil {
+		return nil, err
 	}
+
+	lk := &Lock{
+		logger: l.logger.WithValues("key", key, "id", strs.Hex(6)),
+		holder: l.holder,
+		client: l.client,
+		expiry: l.expiry,
+		key:    key,
+	}
+
+	// Start renew.
+	renewCtx, renewCancel := context.WithCancel(ctx)
+	lk.renewCancel = renewCancel
+	lk.renew(renewCtx, l.renewInterval, l.maxExecuteDuration)
+
+	return lk, nil
 }
 
 // Lock implement the cronjob go-co-op/gocron Lock interface.
 type Lock struct {
-	key         string
-	client      model.ClientSet
-	logger      log.Logger
-	cancelRenew context.CancelFunc
+	logger log.Logger
+	holder string
+	client model.ClientSet
+	expiry time.Duration
+	key    string
+
+	renewCancel context.CancelFunc
 }
 
-// Unlock release the locked key.
-func (l *Lock) Unlock(ctx context.Context) (err error) {
-	l.logger.V(6).Infof("try to unlock key %s", l.key)
-
-	defer func() {
-		if err != nil {
-			l.logger.Warnf("error unlock key %s: %v", l.key, err)
-		}
-		// Cancel renew goroutine.
-		l.cancelRenew()
-	}()
-
-	err = l.deleteLock(ctx, l.key)
-	if err != nil {
-		return err
-	}
-
-	l.logger.V(6).Infof("success unlock key %s", l.key)
-
-	return nil
+// renew keeps alive the lock.
+func (lk *Lock) renew(ctx context.Context, interval, timeout time.Duration) {
+	gopool.Go(func() {
+		_ = wait.PollUntilContextTimeout(ctx, interval, timeout, true, lk.doRenew)
+		lk.logger.V(6).Info("stopped renew")
+	})
 }
 
-// deleteLock delete the lock with key.
-func (l *Lock) deleteLock(ctx context.Context, key string) error {
-	return l.client.WithTx(ctx, func(tx *model.Tx) (err error) {
-		// Lock key for delete.
-		current, err := tx.DistributeLock.Query().
-			Where(distributelock.ID(key)).
-			ForUpdate().
+// doRenew returns true if not found key,
+// otherwise, keeps running until context canceled.
+func (lk *Lock) doRenew(ctx context.Context) (done bool, err error) {
+	err = lk.client.WithTx(ctx, func(tx *model.Tx) error {
+		// Get key.
+		key, err := tx.DistributeLock.Query().
+			Where(
+				distributelock.ID(lk.key),
+				distributelock.Holder(lk.holder)).
 			Only(ctx)
-		if err != nil && !model.IsNotFound(err) {
+		if err != nil {
 			return err
 		}
 
-		// Delete key.
-		err = tx.DistributeLock.DeleteOne(current).
+		if key.ExpireAt < time.Now().Unix() {
+			// Should not renew.
+			return nil
+		}
+
+		// Renew key.
+		expiry := time.Now().Unix() + int64(lk.expiry.Seconds())
+
+		err = tx.DistributeLock.UpdateOne(key).
+			SetExpireAt(expiry).
 			Exec(ctx)
 		if err != nil {
 			return err
 		}
 
-		return
+		lk.logger.V(6).Info("succeeded renew")
+
+		return nil
 	})
+	if err != nil && ctx.Err() == nil {
+		if model.IsNotFound(err) {
+			// Not found, should not renew again.
+			return true, nil
+		}
+
+		lk.logger.Warnf("error renewing, try at next loop: %v", err)
+	}
+
+	return false, ctx.Err()
+}
+
+// Unlock release the lock.
+func (lk *Lock) Unlock(ctx context.Context) error {
+	// Stop renew.
+	defer lk.renewCancel()
+
+	// Delete key.
+	if ctx.Err() != nil {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+	}
+
+	_, err := lk.client.DistributeLocks().Delete().
+		Where(
+			distributelock.ID(lk.key),
+			distributelock.Holder(lk.holder)).
+		Exec(ctx)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		lk.logger.Warnf("error unlocking: %v", err)
+		return err
+	}
+
+	lk.logger.V(6).Info("succeeded unlock")
+
+	return nil
 }
