b27ca7ae9952e053007a1ee16720ca02ea1c8115
thxCode <thxcode0824@gmail.com>
2023-08-28 22:30:16 +0800

    fix: reschedule job twice after updating
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/cron/registry.go
+++ pkg/cron/registry.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"sync"
 
-	settingbus "github.com/seal-io/walrus/pkg/bus/setting"
 	"github.com/seal-io/walrus/pkg/dao/model"
 	"github.com/seal-io/walrus/pkg/settings"
 	"github.com/seal-io/walrus/utils/cron"
@@ -84,66 +83,6 @@ func doRegister(ctx context.Context, mc *model.Client) error {
 		if err != nil {
 			return fmt.Errorf("error scheduling %s job: %w", n, err)
 		}
-	}
-
-	return nil
-}
-
-// Sync observes the cron expr setting changes and re-register jobs.
-func Sync(ctx context.Context, m settingbus.BusMessage) error {
-	logger := log.WithName("task")
-
-	type job struct {
-		Name string
-		Expr Expr
-		Task Task
-	}
-
-	var jobs []job
-
-	for i := 0; i < len(m.Refers); i++ {
-		if m.Refers[i] == nil || m.Refers[i].Name == "" {
-			continue
-		}
-
-		n := m.Refers[i].Name
-
-		c, exist := js[n]
-		if !exist {
-			continue
-		}
-
-		s := settings.Index(n)
-		if s == nil {
-			continue
-		}
-
-		// Get cron expr of the job from transactional model client.
-		v, err := s.Value(ctx, m.TransactionalModelClient)
-		if err != nil {
-			return fmt.Errorf("error gettting job cron expr: %w", err)
-		}
-
-		j := job{Name: n}
-
-		j.Expr, j.Task, err = c(logger.WithValues("createdBy", n), v)
-		if err != nil {
-			return fmt.Errorf("error creating job for %s: %w", n, err)
-		}
-
-		jobs = append(jobs, j)
-	}
-
-	for i := 0; i < len(jobs); i++ {
-		j := jobs[i]
-
-		err := cron.Schedule(j.Name, j.Expr, j.Task)
-		if err != nil {
-			// NB(thxCode): raising error cannot roll back successfully scheduled job in the same for-loop,
-			// so just warn out here.
-			logger.Errorf("error scheduling job for: %v", j.Name, err)
-		}
-		// TODO(thxCode): support rolling back successfully scheduled job.
 	}
 
 	return nil
--- pkg/cron/syncer.go
+++ pkg/cron/syncer.go
@@ -6,10 +6,13 @@ import (
 	"sync"
 	"time"
 
+	"go.uber.org/multierr"
+
 	settingbus "github.com/seal-io/walrus/pkg/bus/setting"
 	"github.com/seal-io/walrus/pkg/dao/model"
 	"github.com/seal-io/walrus/pkg/dao/types/crypto"
-	cronutil "github.com/seal-io/walrus/utils/cron"
+	"github.com/seal-io/walrus/pkg/settings"
+	"github.com/seal-io/walrus/utils/cron"
 	"github.com/seal-io/walrus/utils/log"
 )
 
@@ -82,7 +85,7 @@ func (in *Syncer) Sync(ctx context.Context) error {
 		}
 		cs := string(cst)
 
-		ce, err := cronutil.ParseCronExpr(cs, false)
+		ce, err := cron.ParseCronExpr(cs, false)
 		if err != nil {
 			in.logger.Warnf("cron spec %s is invalid in setting %s: %v", cs, name, err)
 			continue
@@ -114,4 +117,57 @@ func (in *Syncer) Sync(ctx context.Context) error {
 	}
 
 	return nil
+}
+
+// Sync observes the cron expr setting changes,
+// and replaces the jobs that have changed expression.
+func Sync(ctx context.Context, m settingbus.BusMessage) error {
+	logger := log.WithName("task")
+
+	// Merge the errors to return them all at once,
+	// instead of returning the first error.
+	var berr error
+
+	for i := 0; i < len(m.Refers); i++ {
+		if m.Refers[i] == nil || m.Refers[i].Name == "" {
+			continue
+		}
+
+		n := m.Refers[i].Name
+
+		c, exist := js[n]
+		if !exist {
+			continue
+		}
+
+		s := settings.Index(n)
+		if s == nil {
+			continue
+		}
+
+		// Get cron expression value of the job from transactional model client.
+		v, err := s.Value(ctx, m.TransactionalModelClient)
+		if err != nil {
+			berr = multierr.Append(berr, fmt.Errorf("error getting job cron expression: %w", err))
+			continue
+		}
+
+		// Create a job with the new cron expression value.
+		expr, job, err := c(logger.WithValues("createdBy", n), v)
+		if err != nil {
+			berr = multierr.Append(berr, fmt.Errorf("error creating job for %s: %w", n, err))
+			continue
+		}
+
+		// Reschedule the job with the new cron expression.
+		err = cron.Schedule(n, expr, job)
+		if err != nil {
+			berr = multierr.Append(berr, fmt.Errorf("error rescheduling job for %s: %w", n, err))
+			continue
+		}
+
+		logger.Infof("rescheduled job %q with expression %q", n, expr)
+	}
+
+	return berr
 }
