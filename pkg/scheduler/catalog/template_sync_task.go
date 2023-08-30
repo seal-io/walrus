3431a6a84cacf2cc195be7448f739737bdfa2a77
alexcodelf <nanyilikai@hotmail.com>
2023-08-30 17:11:24 +0800

    fix: catalog sync stuck status

--- pkg/scheduler/catalog/template_sync_task.go
+++ pkg/scheduler/catalog/template_sync_task.go
@@ -74,7 +74,14 @@ func (in *TemplateSyncTask) Process(ctx context.Context, args ...any) error {
 			continue
 		}
 
-		uerr = pkgcatalog.SyncTemplates(ctx, in.modelClient, c)
+		serr := pkgcatalog.SyncTemplates(ctx, in.modelClient, c)
+
+		uerr = pkgcatalog.UpdateStatusWithSyncErr(
+			ctx,
+			in.modelClient,
+			c,
+			serr,
+		)
 		if uerr != nil {
 			berr = multierr.Append(berr,
 				fmt.Errorf("error syncing templates of catalog %s: %w",
