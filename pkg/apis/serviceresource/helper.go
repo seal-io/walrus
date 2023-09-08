9a828d2e083dc8c0dcbedd2b59d91ffefb890901
thxCode <thxcode0824@gmail.com>
2023-09-08 20:08:15 +0800

    fix: failed connect resource terminal
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/apis/serviceresource/helper.go
+++ pkg/apis/serviceresource/helper.go
@@ -2,14 +2,14 @@ package serviceresource
 
 import (
 	"context"
-	"strings"
+	"io"
 	"sync"
 
 	"github.com/seal-io/walrus/pkg/apis/runtime"
 	"github.com/seal-io/walrus/utils/json"
 )
 
-func asTermStream(proxy runtime.RequestBidiStream, initWidth, initHeight int32) termStream {
+func asTermStream(proxy *runtime.RequestBidiStream, initWidth, initHeight int32) termStream {
 	resizeCh := make(chan termSize, 2)
 	resizeCh <- termSize{Width: initWidth, Height: initHeight}
 
@@ -30,29 +30,29 @@ type termStream struct {
 	context.Context
 
 	once   *sync.Once
-	proxy  runtime.RequestBidiStream
+	proxy  *runtime.RequestBidiStream
 	resize chan termSize
-}
-
-func (h termStream) Close() error {
-	close(h.resize)
-	return nil
 }
 
 func (h termStream) Read(p []byte) (n int, err error) {
 	for {
 		n, err = h.proxy.Read(p)
 		if err != nil {
-			if !isUnexpectedError(err) {
-				// Send exit to upstream if proxy exit unexpectedly.
-				h.once.Do(func() {
-					n = copy(p, "exit 0\n")
-					err = nil
-				})
+			// Send exit to upstream in case of session leaking.
+			h.once.Do(func() {
+				n = copy(p, "exit 0\n")
+				err = nil
+			})
+
+			// Prevent useless error message being reported,
+			// for example, `use of closed network connection`, `websocket close`.
+			if err != nil {
+				err = io.EOF
 			}
 
 			return
 		}
+
 		// Resize command is something like `#{"width":100,"height":100}#`
 		// without ending \n(line feed) and \r(carriage return) chars.
 		if n >= 24 && p[0] == '#' && p[1] == '{' && p[n-2] == '}' && p[n-1] == '#' {
@@ -73,11 +73,10 @@ func (h termStream) Write(p []byte) (n int, err error) {
 }
 
 func (h termStream) Next() (uint16, uint16, bool) {
-	t, ok := <-h.resize
-	return uint16(t.Width), uint16(t.Height), ok
-}
-
-func isUnexpectedError(err error) bool {
-	errMsg := err.Error()
-	return strings.Contains(errMsg, "use of closed network connection") // Terminated by destination.
+	select {
+	case <-h.proxy.Done():
+		return 0, 0, false
+	case t, ok := <-h.resize:
+		return uint16(t.Width), uint16(t.Height), ok
+	}
 }
