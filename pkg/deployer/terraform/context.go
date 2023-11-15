dd94ce5ec92859bb2f3f174b6ac96abc2894a786
michelia <michelia@seal.io>
2023-11-15 11:58:35 +0800

    refactor: update walrus metadata to context

---
+++ pkg/deployer/terraform/context.go
@@ -0,0 +1,57 @@
+package terraform
+
+import (
+	"github.com/seal-io/walrus/pkg/dao/types/object"
+)
+
+const WalrusContextVariableName = "context"
+
+// Context indicate the walrus related metadata, will set to attribute context while user module include this attribute.
+type Context struct {
+	// Project indicate the project metadata.
+	Project struct {
+		Name string    `json:"name,omitempty"`
+		ID   object.ID `json:"id,omitempty"`
+	} `json:"project,omitempty"`
+
+	// Environment indicate the environment metadata.
+	Environment struct {
+		Name string    `json:"name,omitempty"`
+		ID   object.ID `json:"id,omitempty"`
+		// Namespace is the managed namespace name of an environment,
+		// valid when Kubernetes connector is used in the environment.
+		Namespace string `json:"namespace,omitempty"`
+	} `json:"environment,omitempty"`
+
+	// Resource indicate the resource metadata.
+	Resource struct {
+		Name string    `json:"name,omitempty"`
+		ID   object.ID `json:"id,omitempty"`
+	} `json:"resource,omitempty"`
+}
+
+func NewContext() *Context {
+	return &Context{}
+}
+
+func (c *Context) SetProject(id object.ID, name string) *Context {
+	c.Project.ID = id
+	c.Project.Name = name
+
+	return c
+}
+
+func (c *Context) SetEnvironment(id object.ID, name, namespace string) *Context {
+	c.Environment.ID = id
+	c.Environment.Name = name
+	c.Environment.Namespace = namespace
+
+	return c
+}
+
+func (c *Context) SetResource(id object.ID, name string) *Context {
+	c.Resource.ID = id
+	c.Resource.Name = name
+
+	return c
+}
