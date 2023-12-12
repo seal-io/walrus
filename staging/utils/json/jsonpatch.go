ddd6cf3e55dce98cc80b39bb5e75474b45337ff6
michelia <michelia@seal.io>
2023-12-12 23:27:35 +0800

    refactor: add json patch and string suffix utils

---
+++ staging/utils/json/jsonpatch.go
@@ -0,0 +1,29 @@
+package json
+
+import (
+	jsonpatch "github.com/evanphx/json-patch"
+)
+
+func PatchObject(obj, patchObject any) (any, error) {
+	ob, err := Marshal(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	pb, err := Marshal(patchObject)
+	if err != nil {
+		return nil, err
+	}
+
+	patched, err := jsonpatch.MergePatch(ob, pb)
+	if err != nil {
+		return nil, err
+	}
+
+	err = Unmarshal(patched, obj)
+	if err != nil {
+		return nil, err
+	}
+
+	return obj, nil
+}
