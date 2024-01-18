a2367407a94f95fe93859f71c322132e1456bac4
alexcc <nanyilikai@hotmail.com>
2024-01-18 16:47:25 +0800

    fix: resource stuck in deleting

--- pkg/deployer/terraform/syncer.go
+++ pkg/deployer/terraform/syncer.go
@@ -2,15 +2,21 @@ package terraform
 
 import (
 	"context"
+	"fmt"
 
 	revisionbus "github.com/seal-io/walrus/pkg/bus/resourcerevision"
+	"github.com/seal-io/walrus/pkg/dao"
 	"github.com/seal-io/walrus/pkg/dao/model/resource"
 	"github.com/seal-io/walrus/pkg/dao/types/status"
+	"github.com/seal-io/walrus/utils/log"
+	"github.com/seal-io/walrus/utils/strs"
 )
 
 // SyncResourceRevisionStatus updates the status of the service according to its recent finished service revision.
 func SyncResourceRevisionStatus(ctx context.Context, bm revisionbus.BusMessage) (err error) {
 	var (
+		logger = log.WithName("deployer").WithName("tf")
+
 		mc       = bm.TransactionalModelClient
 		revision = bm.Refer
 	)
@@ -29,8 +35,26 @@ func SyncResourceRevisionStatus(ctx context.Context, bm revisionbus.BusMessage) 
 	if status.ResourceRevisionStatusReady.IsTrue(revision) {
 		switch {
 		case status.ResourceStatusDeleted.IsUnknown(entity):
-			return mc.Resources().DeleteOne(entity).
+			err = mc.Resources().DeleteOne(entity).
 				Exec(ctx)
+			if err == nil {
+				return nil
+			}
+
+			msg := err.Error()
+			// Check dependants.
+			dependants, rerr := dao.GetResourceDependantNames(ctx, mc, entity)
+			if rerr != nil {
+				logger.Errorf("failed to get dependants of resource %s: %v", entity.Name, rerr)
+			}
+
+			if len(dependants) > 0 {
+				msg = fmt.Sprintf("resource to be deleted is the dependency of: %s", strs.Join(", ", dependants...))
+			}
+
+			// Mark resource delete failed.
+			status.ResourceStatusDeleted.False(entity, msg)
+
 		case status.ResourceStatusStopped.IsUnknown(entity):
 			// Stopping -> Stopped.
 			status.ResourceStatusStopped.True(entity, "")
