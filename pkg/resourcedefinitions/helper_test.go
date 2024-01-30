78d4b854f68b8fde3f3be3de8309c5f38d0e5474
thxCode <thxcode0824@gmail.com>
2024-01-30 20:32:11 +0800

    fix: keep required after defaulting in res def
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/resourcedefinitions/helper_test.go
+++ pkg/resourcedefinitions/helper_test.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/seal-io/walrus/utils/json"
 )
 
 func Test_alignSchemas(t *testing.T) {
@@ -72,5 +74,99 @@ func Test_alignSchemas(t *testing.T) {
 		}
 		actualSchemas := alignSchemas(nb, givenSchemasSlice)
 		assert.Equal(t, expectedSchemas, actualSchemas, tc.Name())
+	}
+}
+
+func Test_refillVariableSchemaRef(t *testing.T) {
+	dir := filepath.Join("testdata", "refill_variable_schema_ref")
+
+	testCases, err := os.ReadDir(dir)
+	if err != nil {
+		t.Fatalf("failed to read testdata: %v", err)
+	}
+
+	loader := openapi3.NewLoader()
+
+	for _, tc := range testCases {
+		if !tc.IsDir() {
+			continue
+		}
+
+		var givenSchemas openapi3.Schemas
+		{
+			givenPath := filepath.Join(dir, tc.Name(), "given.yaml")
+			givenOpenAPISchema, err := loader.LoadFromFile(givenPath)
+			if err != nil {
+				t.Fatalf("failed to load given file %q: %v", givenPath, err)
+			}
+
+			if givenOpenAPISchema.Components == nil ||
+				givenOpenAPISchema.Components.Schemas == nil {
+				t.Fatalf("given file %q has no components.schemas", givenPath)
+			}
+			givenSchemas = givenOpenAPISchema.Components.Schemas
+		}
+
+		var expectedSchemas openapi3.Schemas
+		{
+			expectedPath := filepath.Join(dir, tc.Name(), "expected.yaml")
+			expectedOpenAPISchema, err := loader.LoadFromFile(expectedPath)
+			if err != nil {
+				t.Fatalf("failed to load expected file %q: %v", expectedPath, err)
+			}
+
+			if expectedOpenAPISchema.Components == nil ||
+				expectedOpenAPISchema.Components.Schemas == nil {
+				t.Fatalf("expected file %q has no components.schemas", expectedPath)
+			}
+
+			expectedSchemas = expectedOpenAPISchema.Components.Schemas
+		}
+
+		var nb map[string]any
+		{
+			nbJSONPath := filepath.Join(dir, tc.Name(), "nb.json")
+
+			nbJSONBytes, err := os.ReadFile(nbJSONPath)
+			if err != nil {
+				t.Fatalf("failed to read nb.json: %v", err)
+			}
+
+			err = json.Unmarshal(nbJSONBytes, &nb)
+			if err != nil {
+				t.Fatalf("failed to unmarshal nb.json: %v", err)
+			}
+		}
+
+		var defs [][]byte
+		{
+			defPaths, err := filepath.Glob(
+				filepath.Join(dir, tc.Name(), "default.*.json"))
+			if err != nil {
+				t.Fatalf("failed to glob default files: %v", err)
+			}
+
+			defs = make([][]byte, 0, len(defPaths))
+
+			for i := range defPaths {
+				defPath := defPaths[i]
+				defJSONBytes, err := os.ReadFile(defPath)
+				if err != nil {
+					t.Fatalf("failed to read %s: %v", defPath, err)
+				}
+
+				defs = append(defs, defJSONBytes)
+			}
+		}
+
+		if sr, ok := givenSchemas[variablesSchemaKey]; ok {
+			refillVariableSchemaRef(nb[variablesSchemaKey].(map[string]any), "", sr, defs, "", sr.Value)
+		}
+
+		actualSchemas := givenSchemas
+		actualJSONBytes := json.ShouldMarshalIndent(actualSchemas, "", "  ")
+		expectedJSONBytes := json.ShouldMarshalIndent(expectedSchemas, "", "  ")
+
+		assert.Equal(t, string(expectedJSONBytes), string(actualJSONBytes), tc.Name())
 	}
 }
