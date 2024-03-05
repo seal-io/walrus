c9e5b415e13ee1216b97f9e74a902019f7a0d418
michelia <michelia@seal.io>
2024-03-05 17:34:41 +0800

    fix: remove empty field when cli post/update resource

--- pkg/cli/api/body_param.go
+++ pkg/cli/api/body_param.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"reflect"
+
+	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/spf13/pflag"
 
 	"github.com/seal-io/walrus/utils/strs"
@@ -36,4 +39,48 @@ func (b BodyParam) AddFlag(flags *pflag.FlagSet) any {
 func (b BodyParam) OptionName() string {
 	name := b.Name
 	return strs.Dasherize(name)
+}
+
+// Serialize the parameter based on the type and remove empty value.
+func (b BodyParam) Serialize(value any, flagSet *pflag.FlagSet) any {
+	if value == nil {
+		return nil
+	}
+
+	switch b.Type {
+	case openapi3.TypeBoolean, openapi3.TypeInteger, openapi3.TypeNumber, openapi3.TypeString:
+	case ValueTypeObjectID:
+	case ValueTypeArrayBoolean, ValueTypeArrayInt, ValueTypeArrayNumber, ValueTypeArrayString, ValueTypeArrayObject:
+		v := reflect.ValueOf(value)
+		if v.Kind() == reflect.Ptr {
+			v = v.Elem()
+		}
+
+		if (v.Kind() == reflect.Array || v.Kind() == reflect.Slice) &&
+			v.Len() == 0 &&
+			!flagSet.Changed(b.OptionName()) {
+			return nil
+		}
+	case ValueTypeMapStringInt64, ValueTypeMapStringInt32, ValueTypeMapStringInt, ValueTypeMapStringString:
+		v := reflect.ValueOf(value)
+		if v.Kind() == reflect.Ptr {
+			v = v.Elem()
+		}
+
+		if v.Kind() == reflect.Map &&
+			v.Len() == 0 &&
+			!flagSet.Changed(b.OptionName()) {
+			return nil
+		}
+
+	default:
+		v, ok := value.(*ObjectFlag)
+		if ok &&
+			(v == nil || reflect.DeepEqual(*v, ObjectFlag{})) &&
+			!flagSet.Changed(b.OptionName()) {
+			return nil
+		}
+	}
+
+	return value
 }
